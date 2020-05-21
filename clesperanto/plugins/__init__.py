2c48561f2c4ebfe5925237da851e6cdcbf05894f
haesleinhuepf <rhaase@mpi-cbg.de>
2020-05-21 20:22:13 +0200

    added binary_union and binary_intersection

--- clesperanto/plugins/__init__.py
+++ clesperanto/plugins/__init__.py
@@ -2,8 +2,10 @@ from .absolute import absolute
 from .add_images_weighted import add_images_weighted
 from .add_image_and_scalar import add_image_and_scalar
 from .binary_and import binary_and
+from .binary_and import binary_and as binary_intersection
 from .binary_not import binary_not
 from .binary_or import binary_or
+from .binary_or import binary_or as binary_union
 from .binary_subtract import binary_subtract
 from .binary_xor import binary_xor
 from .convolve import convolve
@@ -14,6 +16,8 @@ from .minimum_sphere import minimum_sphere
 from .multiply_matrix import multiply_matrix
 from .set import set
 from .top_hat_sphere import top_hat_sphere
+
+
 
 
 
