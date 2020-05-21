89a8b06e9f317df71740a633ab6ce18ce5ee6f89
haesleinhuepf <rhaase@mpi-cbg.de>
2020-05-21 20:28:16 +0200

    reorganised + added tests

--- tests.py
+++ tests.py
@@ -1,4 +1,3 @@
-from skimage.io import imread
 import clesperanto as cle
 import numpy as np
 
@@ -7,37 +6,22 @@ test = cle.push(np.asarray([
 [1, 1, 1],
 [1, 2, 1],
 [1, 1, 1]
-#    [1, 1, 1, 1, 1, 1, 1],
-#    [1, 1, 1, 1, 1, 1, 1],
-#    [1, 1, 1, 1, 1, 1, 1],
-#    [1, 1, 1, 2, 1, 1, 1],
-#    [1, 1, 1, 1, 1, 1, 1],
-#    [1, 1, 1, 1, 1, 1, 1],
-#    [1, 1, 1, 1, 1, 1, 1]
 ]))
 
 test2 = cle.create(test)
-# cle.set(test2, 4);
-
-print("A test")
-print(cle.pull(test))
-
-print("A test2")
-print(cle.pull(test2))
-
-
 cle.maximum_sphere(test, test2, 1, 1, 1)
 
-print("B test")
-print(cle.pull(test))
+#print("B test")
+#print(cle.pull(test))
 
-print("B test2")
-print(cle.pull(test2))
+#print("B test2")
+#print(cle.pull(test2))
+
+a = cle.pull(test2)
+assert (np.min(a) == 1)
+assert (np.max(a) == 2)
 
 
-
-
-# test = cle.create((5,4,3))
 cle.set(test, 5)
 #print(test)
 #print(cle.pull(test))
@@ -106,6 +90,34 @@ assert (np.min(a) == 0)
 assert (np.max(a) == 1)
 assert (np.mean(a) == 0.25)
 print ("ok binary_not")
+
+
+
+
+
+
+test1 = cle.push(np.asarray([
+    [0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 0],
+    [0, 1, 1, 1, 1, 0],
+    [0, 1, 1, 1, 1, 0],
+    [0, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0]
+]))
+
+test2 = cle.create(test1)
+cle.binary_edge_detection(test1, test2)
+
+print(test2)
+a = cle.pull(test2)
+assert (np.min(a) == 0)
+assert (np.max(a) == 1)
+assert (np.mean(a) - 12 / 36 < 0.001)
+print ("ok binary_edge_detection")
+
+
+
+
 
 
 
@@ -285,22 +297,3 @@ print ("ok copy slice to 3d")
 
 
 
-
-
-
-
-# load data and allocate memory for result
-fly = imread(filename)
-cle_fly = cle.push(fly);
-
-background_subtracted_fly = cle.create(cle_fly.shape)
-
-# subtract background
-cle.top_hat_sphere(cle_fly, background_subtracted_fly, 15, 15, 0)
-
-result = cle.pull(background_subtracted_fly);
-
-assert (np.min(result) == 0)
-assert (np.max(result) != 0)
-assert (np.mean(result) != 0)
-print("ok top_hat_sphere")
