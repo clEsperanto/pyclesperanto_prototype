b5c9634e20fcc5a56b29b0807b3602151585baba
haesleinhuepf <rhaase@mpi-cbg.de>
2020-05-21 19:22:43 +0200

    added binary_or

--- clesperanto/plugins/__init__.py
+++ clesperanto/plugins/__init__.py
@@ -3,6 +3,7 @@ from .add_images_weighted import add_images_weighted
 from .add_image_and_scalar import add_image_and_scalar
 from .binary_and import binary_and
 from .binary_not import binary_not
+from .binary_or import binary_or
 from .maximum_sphere import maximum_sphere
 from .minimum_sphere import minimum_sphere
 from .multiply_matrix import multiply_matrix
