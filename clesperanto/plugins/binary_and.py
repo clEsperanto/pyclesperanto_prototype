f3e237dff2136e3e3d6ed810eb58aeab57d9d03e
haesleinhuepf <rhaase@mpi-cbg.de>
2020-05-21 19:10:47 +0200

    added binary_and

---
+++ clesperanto/plugins/binary_and.py
@@ -0,0 +1,11 @@
+from ..core import execute
+
+def binary_and (src1, src2, dst):
+
+    parameters = {
+        "src1":src1,
+        "src2":src2,
+        "dst":dst
+    }
+
+    execute(__file__, 'binary_and_' + str(len(dst.shape)) + 'd_x.cl', 'binary_and_' + str(len(dst.shape)) + 'd', dst.shape, parameters)
