c6b6763211e689729be2eab8d59ed50f384908cb
haesleinhuepf <rhaase@mpi-cbg.de>
2020-05-27 14:08:13 +0200

    use decorator and creator in add_images_weighted

--- clesperanto/plugins/add_images_weighted.py
+++ clesperanto/plugins/add_images_weighted.py
@@ -1,7 +1,11 @@
 
 from ..core import execute
+from ..core import create_like
+from ..core import plugin_function
+from gputools import OCLArray
 
-def add_images_weighted(input1, input2, output, weight1, weight2):
+@plugin_function(output_creator=create_like)
+def add_images_weighted(input1:OCLArray, input2:OCLArray, output:OCLArray, weight1=1, weight2=1):
     parameters = {
         "src":input1,
         "src1":input2,
@@ -11,3 +15,6 @@ def add_images_weighted(input1, input2, output, weight1, weight2):
     };
 
     execute(__file__, 'add_images_weighted_' + str(len(output.shape)) + 'd_x.cl', 'add_images_weighted_' + str(len(output.shape)) + 'd', output.shape, parameters);
+
+    return output
+
