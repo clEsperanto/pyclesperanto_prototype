9f8ce905c37fb1d8efeca994fb2ad4be6a0a004d
haesleinhuepf <rhaase@mpi-cbg.de>
2020-05-28 20:38:58 +0200

    added erode_sphere

--- tests.py
+++ tests.py
@@ -783,3 +783,64 @@ b = cle.pull(reference)
 assert (np.array_equal(a, b))
 print ("ok erode_sphere")
 
+
+
+
+
+
+test = cle.push_zyx(np.asarray([
+    [
+        [0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0],
+        [0, 1, 1, 1, 0],
+        [0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0]
+    ],[
+        [0, 0, 0, 0, 0],
+        [1, 1, 1, 0, 0],
+        [1, 1, 1, 0, 0],
+        [1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0]
+    ],[
+        [0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0],
+        [0, 1, 1, 1, 0],
+        [0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0]
+    ]
+]))
+
+reference = cle.push_zyx(np.asarray([
+    [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ],[
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ],[
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ]
+]))
+
+result = cle.create(test)
+cle.erode_sphere_slice_by_slice(test, result)
+
+
+a = cle.pull_zyx(result)
+b = cle.pull_zyx(reference)
+
+print(a)
+
+assert (np.array_equal(a, b))
+print ("ok erode_sphere_slice_by_slice")
+
