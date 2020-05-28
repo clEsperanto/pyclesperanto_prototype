90eadc5a0a7fd85478aae39b3a795d5db950c2a9
haesleinhuepf <rhaase@mpi-cbg.de>
2020-05-28 19:24:09 +0200

    added missing pull_zyx to index

--- clesperanto/core/__init__.py
+++ clesperanto/core/__init__.py
@@ -2,5 +2,6 @@ from .radius_to_kernel_size import radius_to_kernel_size
 from .create import create
 from .execute import execute
 from .pull import pull
+from .pull import pull_zyx
 from .push import push
 from .push import push_zyx
