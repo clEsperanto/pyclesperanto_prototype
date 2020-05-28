a949d90ef2bee4cdd6d4f0759c10f2e9f00e2bb1
haesleinhuepf <rhaase@mpi-cbg.de>
2020-05-28 19:20:41 +0200

    added dilate_box_slice_by_slice

--- clesperanto/plugins/__init__.py
+++ clesperanto/plugins/__init__.py
@@ -13,6 +13,8 @@ from .convolve import convolve
 from .copy import copy
 from .copy_slice import copy_slice
 from .dilate_box import dilate_box
+from .dilate_box_slice_by_slice import dilate_box_slice_by_slice
+
 from .maximum_sphere import maximum_sphere
 from .minimum_sphere import minimum_sphere
 from .multiply_matrix import multiply_matrix
--- tests.py
+++ tests.py
@@ -317,7 +317,7 @@ reference = cle.push(np.asarray([
 result = cle.create(test)
 cle.dilate_box(test, result)
 
-print(test2)
+print(result)
 
 a = cle.pull(result)
 b = cle.pull(reference)
@@ -325,3 +325,59 @@ assert (np.array_equal(a, b))
 print ("ok dilate_box")
 
 
+
+test = cle.push_zyx(np.asarray([
+    [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ],[
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ],[
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ]
+]))
+
+reference = cle.push_zyx(np.asarray([
+    [
+        [0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0],
+        [0, 1, 1, 1, 0],
+        [0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0]
+    ],[
+        [0, 0, 0, 0, 0],
+        [1, 1, 1, 0, 0],
+        [1, 1, 1, 0, 0],
+        [1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0]
+    ],[
+        [0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0],
+        [0, 1, 1, 1, 0],
+        [0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0]
+    ]
+]))
+
+result = cle.create(test)
+cle.dilate_box_slice_by_slice(test, result)
+
+
+a = cle.pull_zyx(result)
+b = cle.pull_zyx(reference)
+
+print(a)
+
+assert (np.array_equal(a, b))
+print ("ok dilate_box_slice_by_slice")
