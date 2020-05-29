5eaa4c19ace5325fd15f65694f9fe07c1e3e8e24
haesleinhuepf <rhaase@mpi-cbg.de>
2020-05-29 19:17:13 +0200

    more test cases

--- test_add_images.py
+++ test_add_images.py
@@ -27,3 +27,33 @@ assert(np.equal(result[0].all(), reference[0].all()))
 
 print("with None as output ok")
 print("============================")
+
+
+
+reference = np.asarray([[9, 12, 15]])
+output = cle.add_images_weighted(input1, input2, None, weight1=1, weight2=2)
+result = cle.pull(output)
+assert(np.equal(result[0].all(), reference[0].all()))
+
+print("with named parameters ok")
+print("============================")
+
+
+
+reference = np.asarray([[9, 12, 15]])
+output = cle.add_images_weighted(input1, input2, weight1=1, weight2=2)
+result = cle.pull(output)
+assert(np.equal(result[0].all(), reference[0].all()))
+
+print("with named parameters and missing parameters ok")
+print("============================")
+
+
+reference = np.asarray([[9, 12, 15]])
+output = cle.add_images_weighted(input1, input2, weight2=2, weight1=1)
+result = cle.pull(output)
+assert(np.equal(result[0].all(), reference[0].all()))
+
+print("with named parameters in wrong order and missing parameters ok")
+print("============================")
+
