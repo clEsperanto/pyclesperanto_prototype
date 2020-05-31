f8790d1ca62df42b73a0862a47114de94645c1eb
haesleinhuepf <rhaase@mpi-cbg.de>
2020-05-31 19:10:30 +0200

    added maximum_y_projection

---
+++ clesperanto/plugins/maximum_y_projection.py
@@ -0,0 +1,11 @@
+from ..core import radius_to_kernel_size
+from ..core import execute
+
+def maximum_y_projection(input, output):
+
+    parameters = {
+        "dst_max":output,
+        "src":input,
+    };
+
+    execute(__file__, 'maximum_y_projection_x.cl', 'maximum_y_projection', output.shape, parameters);
