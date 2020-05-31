e6fec4df664d8322b71a42b226e371ae5812f90b
haesleinhuepf <rhaase@mpi-cbg.de>
2020-05-31 19:34:02 +0200

    added onlyzero_overwrite_maxmimum_diamond

---
+++ clesperanto/plugins/onlyzero_overwrite_maximum_diamond.py
@@ -0,0 +1,13 @@
+from ..core import execute
+
+def onlyzero_overwrite_maximum_diamond (src, flag_dst, dst):
+
+    parameters = {
+        "dst": dst,
+        "flag_dst": flag_dst,
+        "src":src,
+    }
+
+    execute(__file__, 'onlyzero_overwrite_maximum_diamond_' + str(len(dst.shape)) + 'd_x.cl', 'onlyzero_overwrite_maximum_diamond_' + str(len(dst.shape)) + 'd', dst.shape, parameters)
+
+    return [flag_dst, dst]
--- tests.py
+++ tests.py
@@ -2329,6 +2329,37 @@ print ("ok onlyzero_overwrite_maximum_box")
 
 
 
+test1 = cle.push(np.asarray([
+    [0, 0, 0, 0, 0],
+    [0, 1, 2, 3, 0],
+    [0, 2, 3, 4, 0],
+    [0, 4, 4, 5, 0],
+    [0, 0, 0, 0, 0]
+]))
+
+reference = cle.push(np.asarray([
+    [0, 1, 2, 3, 0],
+    [1, 1, 2, 3, 3],
+    [2, 2, 3, 4, 4],
+    [4, 4, 4, 5, 5],
+    [0, 4, 4, 5, 0]
+]))
+
+result = cle.create(test1)
+flag = cle.create((1, 1, 1))
+cle.onlyzero_overwrite_maximum_diamond(test1, flag, result)
+
+print(result)
+
+a = cle.pull(result)
+b = cle.pull(reference)
+assert (np.array_equal(a, b))
+print ("ok onlyzero_overwrite_maximum_diamond")
+
+
+
+
+
 
 
 
