307d128be85a882a6640abe09dbdccf63d71a925
haesleinhuepf <rhaase@mpi-cbg.de>
2020-05-31 18:41:04 +0200

    added nonzero_maximum_diamond

--- clesperanto/plugins/__init__.py
+++ clesperanto/plugins/__init__.py
@@ -54,6 +54,7 @@ from .multiply_image_and_coordinate import multiply_image_and_coordinate
 from .multiply_image_and_scalar import multiply_image_and_scalar
 from .multiply_images import multiply_images
 from .nonzero_maximum_box import nonzero_maximum_box
+from .nonzero_maximum_diamond import nonzero_maximum_diamond
 from .nonzero_minimum_box import nonzero_minimum_box
 from .nonzero_minimum_diamond import nonzero_minimum_diamond
 from .not_equal import not_equal
