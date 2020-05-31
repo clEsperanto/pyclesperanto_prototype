0bb73f87b7d8e43f7a9991504bb9737a58fe7abf
haesleinhuepf <rhaase@mpi-cbg.de>
2020-05-31 20:01:37 +0200

    added set_plane

---
+++ clesperanto/plugins/set_plane.py
@@ -0,0 +1,10 @@
+from ..core import execute
+
+def set_plane(output, plane, scalar):
+    parameters = {
+        "dst":output,
+        "plane":int(plane),
+        "value":float(scalar)
+    }
+
+    execute(__file__, 'set_plane_' + str(len(output.shape)) + 'd_x.cl', 'set_plane_' + str(len(output.shape)) + 'd', output.shape, parameters);
--- tests.py
+++ tests.py
@@ -2563,6 +2563,50 @@ print ("ok set_column")
 
 
 
+result = cle.push(np.asarray([
+    [
+    [3, 3, 3, 3, 3],
+    [3, 3, 3, 3, 3],
+    [3, 3, 3, 3, 3],
+    [3, 3, 3, 3, 3],
+    [3, 3, 3, 3, 3]
+        ],[
+    [3, 3, 3, 3, 3],
+    [3, 3, 3, 3, 3],
+    [3, 3, 3, 3, 3],
+    [3, 3, 3, 3, 3],
+    [3, 3, 3, 3, 3]
+        ]
+]))
+
+reference = cle.push(np.asarray([
+    [
+        [3, 4, 3, 3, 3],
+        [3, 4, 3, 3, 3],
+        [3, 4, 3, 3, 3],
+        [3, 4, 3, 3, 3],
+        [3, 4, 3, 3, 3]
+    ], [
+        [3, 4, 3, 3, 3],
+        [3, 4, 3, 3, 3],
+        [3, 4, 3, 3, 3],
+        [3, 4, 3, 3, 3],
+        [3, 4, 3, 3, 3]
+    ]
+]))
+
+cle.set_plane(result, 1, 4)
+
+print(result)
+
+a = cle.pull(result)
+b = cle.pull(reference)
+assert (np.allclose(a, b, 0.001))
+print ("ok set_plane")
+
+
+
+
 
 
 result = cle.push(np.asarray([
