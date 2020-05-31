95b60c33069853c966a66e7d5ebf12c7f7d05b96
haesleinhuepf <rhaase@mpi-cbg.de>
2020-05-31 22:26:52 +0200

    added sum_z_projection

---
+++ clesperanto/plugins/sum_z_projection.py
@@ -0,0 +1,11 @@
+from ..core import radius_to_kernel_size
+from ..core import execute
+
+def sum_z_projection(input, output):
+
+    parameters = {
+        "dst":output,
+        "src":input,
+    };
+
+    execute(__file__, 'sum_z_projection_x.cl', 'sum_z_projection', output.shape, parameters);
