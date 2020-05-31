7cc407d74577b1e864cf7631ce83d65ab6c6ef58
haesleinhuepf <rhaase@mpi-cbg.de>
2020-05-31 22:50:39 +0200

    added set_where_x_smaller_than_y

--- tests.py
+++ tests.py
@@ -2798,6 +2798,34 @@ print ("ok set_nonzero_pixels_to_pixelindex")
 
 
 
+result = cle.push(np.asarray([
+    [0, 0, 0, 1],
+    [0, 0, 3, 1],
+    [0, 0, 3, 1],
+    [1, 1, 1, 1]
+]))
+
+reference = cle.push(np.asarray([
+    [0, 3, 3, 3],
+    [0, 0, 3, 3],
+    [0, 0, 3, 3],
+    [1, 1, 1, 1]
+]))
+
+cle.set_where_x_smaller_than_y(result, 3)
+
+a = cle.pull(result)
+b = cle.pull(reference)
+print(a)
+
+assert (np.array_equal(a, b))
+print ("ok set_where_x_smaller_than_y")
+
+
+
+
+
+
 
 
 test1 = cle.push(np.asarray([
