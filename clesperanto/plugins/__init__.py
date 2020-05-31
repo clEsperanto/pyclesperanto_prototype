447c93c1b15fe8da9eeee033f68e1ebaf28ae3d7
haesleinhuepf <rhaase@mpi-cbg.de>
2020-05-31 19:07:42 +0200

    added maximum_z_projection

--- clesperanto/plugins/__init__.py
+++ clesperanto/plugins/__init__.py
@@ -45,6 +45,7 @@ from .mask_label import mask_label
 from .maximum_image_and_scalar import maximum_image_and_scalar
 from .maximum_images import maximum_images
 from .maximum_box import maximum_box
+from .maximum_z_projection import maximum_z_projection
 from .mean_box import mean_box
 from .mean_sphere import mean_sphere
 from .minimum_box import minimum_box
---
+++ clesperanto/plugins/maximum_z_projection.py
@@ -0,0 +1,11 @@
+from ..core import radius_to_kernel_size
+from ..core import execute
+
+def maximum_z_projection(input, output):
+
+    parameters = {
+        "dst_max":output,
+        "src":input,
+    };
+
+    execute(__file__, 'maximum_z_projection_x.cl', 'maximum_z_projection', output.shape, parameters);
