e2143b1260416195770e9508d84f2add6a4a290b
haesleinhuepf <rhaase@mpi-cbg.de>
2020-05-31 22:24:37 +0200

    added standard_deviation_z_projection

--- clesperanto/plugins/__init__.py
+++ clesperanto/plugins/__init__.py
@@ -88,6 +88,7 @@ from .smaller_constant import smaller_constant
 from .smaller_or_equal import smaller_or_equal
 from .smaller_or_equal_constant import smaller_or_equal_constant
 from .sobel import sobel
+from .standard_deviation_z_projection import standard_deviation_z_projection
 from .top_hat_sphere import top_hat_sphere
 
 
---
+++ clesperanto/plugins/standard_deviation_z_projection.py
@@ -0,0 +1,11 @@
+from ..core import radius_to_kernel_size
+from ..core import execute
+
+def standard_deviation_z_projection(input, output):
+
+    parameters = {
+        "dst":output,
+        "src":input,
+    };
+
+    execute(__file__, 'standard_deviation_z_projection_x.cl', 'standard_deviation_z_projection', output.shape, parameters);
