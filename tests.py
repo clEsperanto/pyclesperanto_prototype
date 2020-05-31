09dd052d967194aba3c8f343bd1e85b0ece5c7f9
haesleinhuepf <rhaase@mpi-cbg.de>
2020-05-31 22:04:44 +0200

    added set_ramp_z

--- tests.py
+++ tests.py
@@ -2686,6 +2686,50 @@ print ("ok set_ramp_y")
 
 
 
+
+
+
+
+
+result = cle.push(np.asarray([
+    [
+        [0, 0, 0],
+        [3, 4, 3],
+        [3, 4, 3]
+    ],[
+        [3, 4, 3],
+        [3, 4, 3],
+        [3, 4, 3]
+    ]
+]))
+
+reference = cle.push(np.asarray([
+    [
+        [0, 1, 2],
+        [0, 1, 2],
+        [0, 1, 2]
+    ], [
+        [0, 1, 2],
+        [0, 1, 2],
+        [0, 1, 2]
+    ]
+]))
+
+cle.set_ramp_z(result)
+
+a = cle.pull(result)
+b = cle.pull(reference)
+
+print(a)
+
+assert (np.allclose(a, b, 0.001))
+print ("ok set_ramp_z")
+
+
+
+
+
+
 result = cle.push(np.asarray([
     [3, 3, 3, 3, 3],
     [3, 3, 3, 3, 3],
