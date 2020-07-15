12ed40d5e9a550eefec3ed4222ac1c25b067abc4
haesleinhuepf <rhaase@mpi-cbg.de>
2020-07-15 20:06:01 +0200

    correct version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: BSD3 License",
+        "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
