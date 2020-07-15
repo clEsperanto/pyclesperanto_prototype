b3e2d0dbd00d08c252cc6577331a5d8066a176c0
Talley Lambert <talley.lambert@gmail.com>
2020-07-15 17:30:00 -0400

    cleanup execute

--- clesperanto/core/execute.py
+++ clesperanto/core/execute.py
@@ -1,125 +1,108 @@
+from functools import lru_cache
+from pathlib import Path
 
 import numpy as np
-from gputools import OCLProgram
-import pyopencl
-import os
 
-def execute(anchor, opencl_kernel_filename, kernel_name, global_size, parameters):
-    '''
-    Convenience method for calling opencl kernel files
+import pyopencl as cl
+from gputools import OCLProgram
+
+
+# should write a test to make sure kernels and filenames always match
+KERNEL_FILE_PATTERN = "{kernel_name}_{ndim}d_x.cl"
+
+
+@lru_cache(maxsize=1)
+def preamble():
+    return (Path(__file__).parent / "preamble.cl").read_text()
+
+
+@lru_cache(maxsize=128)
+def get_ocl_source(kernel_name, ndim):
+    from .. import plugins
+
+    kernel_store = Path(plugins.__file__).parent
+    fname = KERNEL_FILE_PATTERN.format(kernel_name=kernel_name, ndim=ndim)
+    kernel = (kernel_store / fname).read_text()
+    return "\n".join([preamble(), kernel])
+
+
+def execute(kernel_name, global_size, parameters):
+    """Convenience method for calling opencl kernel files.
 
     This method basically does the same as the CLKernelExecutor in CLIJ:
     https://github.com/clij/clij-clearcl/blob/master/src/main/java/net/haesleinhuepf/clij/clearcl/util/CLKernelExecutor.java
 
-    :param anchor: Enter __file__ when calling this method and the corresponding open.cl file lies in the same folder as the python file calling it.
-    :param opencl_kernel_filename: Filename of the open.cl file
+    :param anchor: Enter __file__ when calling this method and the corresponding open.cl
+                   file lies in the same folder as the python file calling it.
     :param kernel_name: kernel method inside the open.cl file to be called
-    :param global_size: global_size according to OpenCL definition (usually size of the destination image.
-    :param parameters: dictionary containing parameters. Take care: They must be of the right type
+    :param global_size: global_size according to OpenCL definition (usually size of the
+                        destination image.)
+    :param parameters: dictionary containing parameters. Take care: They must be of the
+                       right type
     :return:
-    '''
-    anchor_path = os.path.dirname(os.path.realpath(anchor)).replace("\\", "/") + "/"
-    cle_path = os.path.dirname(os.path.realpath(__file__)).replace("\\", "/") + "/"
-    ocl_code = __readFile(cle_path + 'preamble.cl') + '\n' + __readFile(anchor_path + opencl_kernel_filename)
-    #print(ocl_code)
+    """
 
-    defines = ""
-    defines = defines + '\n#define GET_IMAGE_WIDTH(image_key) IMAGE_SIZE_ ## image_key ## _WIDTH'
-    defines = defines + '\n#define GET_IMAGE_HEIGHT(image_key) IMAGE_SIZE_ ## image_key ## _HEIGHT'
-    defines = defines + '\n#define GET_IMAGE_DEPTH(image_key) IMAGE_SIZE_ ## image_key ## _DEPTH'
+    defines = [
+        "#define GET_IMAGE_WIDTH(image_key) IMAGE_SIZE_ ## image_key ## _WIDTH",
+        "#define GET_IMAGE_HEIGHT(image_key) IMAGE_SIZE_ ## image_key ## _HEIGHT",
+        "#define GET_IMAGE_DEPTH(image_key) IMAGE_SIZE_ ## image_key ## _DEPTH",
+    ]
 
     arguments = []
 
-    for key in parameters:
-        #print(key)
-        value = parameters[key]
-        #print(value)
-        if (isinstance(value, pyopencl.array.Array)):
+    for key, value in parameters.items():
+
+        if isinstance(value, cl.array.Array):
             arguments.append(value.data)
 
-            if(value.dtype ==  np.dtype('float32') ):
-                typeId = 'f'
+            if value.dtype != np.dtype("float32"):
+                raise TypeError(
+                    "Only float32 is currently supported for images/buffers/arrays"
+                )
 
-                # image type handling
-                defines = defines + '\n#define CONVERT_' + key + '_PIXEL_TYPE clij_convert_float_sat'
-                defines = defines + '\n#define IMAGE_' + key + '_TYPE __global float*'
-                defines = defines + '\n#define IMAGE_' + key + '_PIXEL_TYPE float'
+            # image type handling
+            typeId = "f"
+            ndim = value.ndim
+            depth_height_width = [1, 1, 1]
+            depth_height_width[-len(value.shape) :] = value.shape
+            depth, height, width = depth_height_width
+            pos_type = "int2" if ndim < 3 else "int4"
+            pos = ["(pos0, 0)", "(pos0, pos1)", "(pos0, pos1, pos2, 0)"][ndim - 1]
+            img_dims = 2 if ndim < 3 else 2
 
-                # image size handling
-                if (value.ndim > 2):
-                    #print("dim3:")
-                    #print(value.shape)
-                    defines = defines + '\n#define IMAGE_SIZE_' + key + '_WIDTH ' + str(value.shape[2])
-                    defines = defines + '\n#define IMAGE_SIZE_' + key + '_HEIGHT ' + str(value.shape[1])
-                    defines = defines + '\n#define IMAGE_SIZE_' + key + '_DEPTH ' + str(value.shape[0])
+            defines.extend(
+                [
+                    f"#define CONVERT_{key}_PIXEL_TYPE clij_convert_float_sat",
+                    f"#define IMAGE_{key}_TYPE __global float*",
+                    f"#define IMAGE_{key}_PIXEL_TYPE float",
+                    f"#define IMAGE_SIZE_{key}_WIDTH {width}",
+                    f"#define IMAGE_SIZE_{key}_HEIGHT {height}",
+                    f"#define IMAGE_SIZE_{key}_DEPTH {depth}",
+                    f"#define POS_{key}_TYPE {pos_type}",
+                    f"#define POS_{key}_INSTANCE(pos0,pos1,pos2,pos3) ({pos_type}){pos}",
+                    f"#define READ_{key}_IMAGE(a,b,c) read_buffer{img_dims}d{typeId}"  # !
+                    "(GET_IMAGE_WIDTH(a),GET_IMAGE_HEIGHT(a),GET_IMAGE_DEPTH(a),a,b,c)",
+                    f"#define WRITE_{key}_IMAGE(a,b,c) write_buffer{img_dims}d{typeId}"  # !
+                    "(GET_IMAGE_WIDTH(a),GET_IMAGE_HEIGHT(a),GET_IMAGE_DEPTH(a),a,b,c)",
+                ]
+            )
 
-                else:
-                    if (value.ndim > 1):
-                        #print("dim2")
-
-                        defines = defines + '\n#define IMAGE_SIZE_' + key + '_WIDTH ' + str(value.shape[1])
-                        defines = defines + '\n#define IMAGE_SIZE_' + key + '_HEIGHT ' + str(value.shape[0])
-                    else:
-                        #print("dim1")
-                        defines = defines + '\n#define IMAGE_SIZE_' + key + '_WIDTH ' + str(value.shape[0])
-                        defines = defines + '\n#define IMAGE_SIZE_' + key + '_HEIGHT 1'
-
-                    defines = defines + '\n#define IMAGE_SIZE_' + key + '_DEPTH 1'
-
-                # positions (dimensionality) handling
-                if (value.ndim < 3):
-                    defines = defines + '\n#define POS_' + key + '_TYPE int2'
-                    if (value.ndim == 1):
-                        defines = defines + '\n#define POS_' + key + '_INSTANCE(pos0,pos1,pos2,pos3) (int2)(pos0, 0)'
-                    else:
-                        defines = defines + '\n#define POS_' + key + '_INSTANCE(pos0,pos1,pos2,pos3) (int2)(pos0, pos1)'
-
-                else:
-                    defines = defines + '\n#define POS_' + key + '_TYPE int4'
-                    defines = defines + '\n#define POS_' + key + '_INSTANCE(pos0,pos1,pos2,pos3) (int4)(pos0, pos1, pos2, 0)'
-
-                # read/write images
-                dimensions = 3
-                if (value.ndim < 3):
-                    dimensions = 2
-
-                defines = defines + '\n#define READ_' + key + '_IMAGE(a,b,c) read_buffer' + str(dimensions) + 'd' + typeId + '(GET_IMAGE_WIDTH(a),GET_IMAGE_HEIGHT(a),GET_IMAGE_DEPTH(a),a,b,c)'
-                defines = defines + '\n#define WRITE_' + key + '_IMAGE(a,b,c) write_buffer' + str(dimensions) + 'd' + typeId + '(GET_IMAGE_WIDTH(a),GET_IMAGE_HEIGHT(a),GET_IMAGE_DEPTH(a),a,b,c)'
-
-            else:
-                raise TypeError('other types than float32 aren`t supported yet for images/buffers/arrays')
+        elif isinstance(value, int):
+            arguments.append(np.array([value], np.int))
+        elif isinstance(value, float):
+            arguments.append(np.array([value], np.float32))
         else:
-            if (isinstance(value, int) ):
-                arguments.append(np.array([value], np.int))
-            elif (isinstance(value, float)):
-                arguments.append(np.array([value], np.float32))
-            else:
-                raise TypeError('other types than float and int aren`t supported yet for parameters')
+            raise TypeError(
+                "other types than float and int aren`t supported yet for parameters"
+            )
 
-
-    defines = defines + '\n'
-    ocl_code = defines + ocl_code;
-    #print(defines)
-
-
-    if (len(global_size) == 2):
-        global_size = (global_size[1], global_size[0])
-    else:
-        global_size = (global_size[2], global_size[1], global_size[0])
-
-    #print("global_size")
-    #print(global_size)
+    ndims = len(global_size)
+    defines.append(get_ocl_source(kernel_name, ndims))
+    ocl_code = "\n".join(defines)
 
     prog = OCLProgram(src_str=ocl_code)
-    prog.run_kernel(kernel_name, global_size, None, *arguments) # Todo: the order of the arguments matters; fix that
+    # Todo: the order of the arguments matters; fix that
 
-
-
-########################################################################################################################
-# internal stuff
-
-def __readFile(filename):
-    with open(filename, 'r') as myfile:
-        data = myfile.read()
-    return data
-
+    prog.run_kernel(
+        f"{kernel_name}_{ndims}d", tuple(global_size[::-1]), None, *arguments
+    )
--- clesperanto/plugins/add_image_and_scalar.py
+++ clesperanto/plugins/add_image_and_scalar.py
@@ -20,10 +20,5 @@ def add_image_and_scalar(input, output, scalar):
 
     """
 
-
-    parameters = {
-        "src":input,
-        "dst":output,
-        "scalar":float(scalar)
-    };
-    execute(__file__, 'add_image_and_scalar_' + str(len(output.shape)) + 'd_x.cl', 'add_image_and_scalar_' + str(len(output.shape)) + 'd', output.shape, parameters);
+    parameters = {"src": input, "dst": output, "scalar": float(scalar)}
+    execute("add_image_and_scalar", output.shape, parameters)
