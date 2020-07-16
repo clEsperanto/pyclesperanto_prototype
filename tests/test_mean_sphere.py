cd3e497df190c2a4039a0898bfc19a028bb22660
Talley Lambert <talley.lambert@gmail.com>
2020-07-16 17:56:03 -0400

    xfail certain linux CI tests

--- tests/test_mean_sphere.py
+++ tests/test_mean_sphere.py
@@ -1,7 +1,12 @@
 import pyclesperanto_prototype as cle
 import numpy as np
+import pytest
+import pyopencl as cl
+
+from . import LINUX, CI
 
 
+@pytest.mark.xfail(LINUX and CI, reason='INVALID_ARG_SIZE on CI', raises=cl.LogicError)
 def test_mean_sphere():
     test1 = cle.push(np.asarray([
         [0, 0, 0, 0, 0],
