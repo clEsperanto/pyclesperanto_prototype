3aa3b454177bd0b3b4000b1ce4feee49a3a6a1bf
Robert Haase <haesleinhuepf@users.noreply.github.com>
2020-07-17 21:08:03 +0200

    corrected reference in test

--- tests/test_touch_matrix_to_mesh.py
+++ tests/test_touch_matrix_to_mesh.py
@@ -16,21 +16,23 @@ def test_touch_matrix_to_mesh():
 
     gpu_output = cle.create([5, 5])
 
-    gpu_reference = cle.push_zyx(np.asarray([
+    gpu_reference = cle.push(np.asarray([
                     [0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0],
-                    [0, 1, 0, 0, 0],
                     [0, 0, 1, 0, 0],
-                    [0, 0, 0, 1, 0]
+                    [0, 0, 0, 1, 0],
+                    [0, 0, 0, 0, 1],
+                    [0, 0, 0, 0, 0]
     ]))
 
 
 
     cle.touch_matrix_to_mesh(gpu_point_list, gpu_touch_matrix, gpu_output)
-    print(gpu_output)
 
     a = cle.pull(gpu_output)
     b = cle.pull(gpu_reference)
+
+    print(a)
+    print(b)
 
 
     assert (np.array_equal(a, b))
