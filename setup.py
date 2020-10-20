2134dd9fd50878b796bcaa5e73e87b983d2df7b0
Robert Haase <haesleinhuepf@users.noreply.github.com>
2020-10-20 15:41:04 +0200

    bugfix attempt: CI

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     url="https://github.com/clEsperanto/pyclesperanto_prototype",
     packages=setuptools.find_packages(),
     include_package_data=True,
-    install_requires=["numpy", "pyopencl", "toolz"],
+    install_requires=["numpy", "pyopencl", "toolz", "scikit-image"],
     python_requires='>=3.7',
     classifiers=[
         "Programming Language :: Python :: 3",
