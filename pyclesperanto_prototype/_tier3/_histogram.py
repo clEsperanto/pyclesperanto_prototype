e447619749565bf5536b7ff2c37724756242152f
Robert Haase <haesleinhuepf@users.noreply.github.com>
2020-10-20 11:46:47 +0200

    WIP: threshold_otsu

--- pyclesperanto_prototype/_tier3/_histogram.py
+++ pyclesperanto_prototype/_tier3/_histogram.py
@@ -14,6 +14,7 @@
 #                           http://www.openclprogrammingguide.com
 from .._tier0 import execute
 from .._tier0 import create
+from .._tier0 import pull
 from .._tier0 import plugin_function
 from .._tier0 import Image
 from .._tier2 import minimum_of_all_pixels
@@ -26,16 +27,19 @@ def histogram(image : Image, hist : Image = None, num_bins = 256, minimum_intens
     """
     documentation placeholder
     """
+    image_to_process = image
 
     # workaround for 2D images; the 2D kernel doesn't work as
     # in Java (global_id starts a 1 instead of 0, only tested
     # on AMD Ryzen 4700U, Vega 7)
     # thus, we copy the 2D image in a 3D stack with one slice
-    if (len(image.shape) == 2):
-        temp = image
-        image =create([1, temp.shape[0], temp.shape[1]])
-        copy_slice(temp, image, 0)
+    if (len(image_to_process.shape) == 2):
+        temp = image_to_process
+        image_to_process = create([1, temp.shape[0], temp.shape[1]])
+        copy_slice(temp, image_to_process, 0)
 
+    # print("image shape " + str(image_to_process.shape))
+    # print(str(pull(image_to_process)[0]))
 
     if minimum_intensity is None or maximum_intensity is None or determine_min_max:
         minimum_intensity = minimum_of_all_pixels(image)
@@ -47,7 +51,7 @@ def histogram(image : Image, hist : Image = None, num_bins = 256, minimum_intens
     partial_histograms = create([number_of_partial_histograms, 1, num_bins])
 
     parameters = {
-        "src":image,
+        "src":image_to_process,
         "dst_histogram":partial_histograms,
         "minimum": float(minimum_intensity),
         "maximum": float(maximum_intensity),
@@ -62,15 +66,15 @@ def histogram(image : Image, hist : Image = None, num_bins = 256, minimum_intens
 
     global_sizes = [number_of_partial_histograms]
     execute(__file__,
-                "histogram_" + str(len(image.shape)) + "d_x.cl",
-                "histogram_" + str(len(image.shape)) + "d",
+                "histogram_" + str(len(image_to_process.shape)) + "d_x.cl",
+                "histogram_" + str(len(image_to_process.shape)) + "d",
                 global_sizes,
                 parameters,
                 constants=constants)
 
     # sum partial histograms
     if hist is None:
-        hist = create([num_bins, 1, 1])
+        hist = create([1, 1, num_bins]) # todo: this line should not be necessary if plugin_type does its job
 
     sum_z_projection(partial_histograms, hist)
 
