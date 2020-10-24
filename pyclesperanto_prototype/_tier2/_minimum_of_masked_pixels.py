5eed65656df533080f639ccd93f669d3b6419ec3
Robert Haase <haesleinhuepf@users.noreply.github.com>
2020-10-24 16:29:31 +0200

    bugfixes in minimum_of_masked_pixels it didn't work with 2d images because there is an offset - set by PyOpenCL?

--- pyclesperanto_prototype/_tier2/_minimum_of_masked_pixels.py
+++ pyclesperanto_prototype/_tier2/_minimum_of_masked_pixels.py
@@ -1,8 +1,12 @@
 from .._tier0 import Image
+from .._tier0 import create
 from .._tier0 import create_2d_xy
+from .._tier0 import create_2d_yx
 from .._tier0 import execute
 from .._tier1 import transpose_xz
+from .._tier1 import transpose_yz
 from .._tier0 import pull
+from .._tier1 import copy_slice
 
 def __minimum_of_masked_pixels_reduction(input : Image, input_mask : Image, output : Image, output_mask : Image):
     parameters = {
@@ -12,14 +16,27 @@ def __minimum_of_masked_pixels_reduction(input : Image, input_mask : Image, outp
         "src": input
     }
 
-    execute(__file__, 'minimum_of_masked_pixels_3d_2d_x.cl', 'minimum_of_masked_pixels_3d_2d', output.shape,
-            parameters)
+    execute(__file__, 'minimum_of_masked_pixels_3d_2d_x.cl', 'minimum_of_masked_pixels_3d_2d', output.shape, parameters)
 
+def _slice_to_stack(image : Image, stack : Image = None, num_slices = 1):
+    # TODO: This might become a core method
+    if stack is None:
+        stack = create([num_slices, image.shape[0], image.shape[1]])
+
+    for i in range(0, num_slices):
+        copy_slice(image, stack, i)
+    return stack
 
 def minimum_of_masked_pixels(input : Image, mask : Image):
+    """
+
+    :param input:
+    :return:
+    """
 
     dimensionality = input.shape
 
+    # analyse a 3D image by reducing it to 2D using special minimum projection
     if (len(dimensionality) == 3): # 3D image
         reduced_image = create_2d_xy(input)
         reduced_mask = create_2d_xy(mask)
@@ -29,23 +46,28 @@ def minimum_of_masked_pixels(input : Image, mask : Image):
         input = reduced_image
         mask = reduced_mask
 
+    # analyse the 2D image by making a stack out of it first
     if (len(dimensionality) == 2): # 2D image
         temp_input = transpose_xz(input)
         temp_mask = transpose_xz(mask)
-        reduced_image = create_2d_xy(temp_input)
-        reduced_mask = create_2d_xy(temp_mask)
+
+        reduced_image = create_2d_yx(temp_input)
+        reduced_mask = create_2d_yx(temp_mask)
 
         __minimum_of_masked_pixels_reduction(temp_input, temp_mask, reduced_image, reduced_mask)
 
-        input = reduced_image
-        mask = reduced_mask
+        input = _slice_to_stack(reduced_image)
+        mask = _slice_to_stack(reduced_mask)
 
-    temp_input = transpose_xz(input)
-    temp_mask = transpose_xz(mask)
-    reduced_image = create_2d_xy(temp_input)
-    reduced_mask = create_2d_xy(temp_mask)
+    # analyse a 1D image by making a stack out of it first
+    temp_input = transpose_yz(input)
+    temp_mask = transpose_yz(mask)
+
+    reduced_image = create([1,1])
+    reduced_mask = create([1,1])
 
     __minimum_of_masked_pixels_reduction(temp_input, temp_mask, reduced_image, reduced_mask)
 
-    return pull(reduced_image)[0]
+    # return the single pixel value
+    return (pull(reduced_image)[0])[0]
 
--- tests/test_minimum_of_masked_pixels.py
+++ tests/test_minimum_of_masked_pixels.py
@@ -1,7 +1,29 @@
 import pyclesperanto_prototype as cle
 import numpy as np
 
-def test_minimum_of_all_pixels():
+def test_minimum_of_masked_pixels_mini_x():
+    np_input = np.asarray([[1, 2, 3, 4]])
+    np_mask = np.asarray([[0, 1, 1, 0]])
+
+    gpu_input = cle.push_zyx(np_input)
+    gpu_mask = cle.push_zyx(np_mask)
+
+    result = cle.minimum_of_masked_pixels(gpu_input, gpu_mask)
+    print(result)
+    assert (result == 1)
+
+def test_minimum_of_masked_pixels_mini_y():
+    np_input = np.asarray([[1], [2], [3], [4]])
+    np_mask = np.asarray([[0], [1], [1], [0]])
+
+    gpu_input = cle.push_zyx(np_input)
+    gpu_mask = cle.push_zyx(np_mask)
+
+    result = cle.minimum_of_masked_pixels(gpu_input, gpu_mask)
+    print(result)
+    assert (result == 1)
+
+def test_minimum_of_masked_pixels():
     np_input = np.asarray([
         [
             [1, 2, 3, 10],
