764dcb6b002de5b918ce23928ba081b7a747107a
Robert Haase <haesleinhuepf@users.noreply.github.com>
2020-10-24 15:35:23 +0200

    more+better tests for copy_slice

--- tests/test_copy_slice.py
+++ tests/test_copy_slice.py
@@ -33,8 +33,8 @@ def test_copy_slice_from_3d():
 @pytest.mark.xfail(reason="BUILD_PROGRAM_FAILURE")
 def test_copy_slice_to_3d():
     test1 = cle.push(np.asarray([
-            [4, 4],
-            [4, 4]
+            [3, 4],
+            [4, 5]
     ]))
 
     test2 = cle.create((2, 2, 2))
@@ -47,3 +47,45 @@ def test_copy_slice_to_3d():
     assert (np.max(a) == 4)
     assert (np.mean(a) == 2)
     print ("ok copy slice to 3d")
+
+def test_copy_slice_to3d_with_one_slice():
+    test1 = cle.push(np.asarray([
+        [3, 4, 6],
+        [4, 5, 2]
+    ]))
+
+    print(test1)
+    print("shape test1 " + str(test1.shape))
+
+    test2 = cle.create((1, 3, 2))
+    print("shape test2 " + str(test2.shape))
+    print(test2)
+
+    cle.copy_slice(test1, test2, 0)
+    print(test2)
+
+    a = cle.pull(test2)
+    assert (np.min(a) == 2)
+    assert (np.max(a) == 6)
+    assert (np.mean(a) == 4)
+
+def test_copy_slice_to3d_with_one_slice_zyx():
+    test1 = cle.push_zyx(np.asarray([
+        [3, 4, 6],
+        [4, 5, 2]
+    ]))
+
+    print(test1)
+    print("shape test1 " + str(test1.shape))
+
+    test2 = cle.create_zyx((1, 3, 2))
+    print("shape test2 " + str(test2.shape))
+    print(test2)
+
+    cle.copy_slice(test1, test2, 0)
+    print(test2)
+
+    a = cle.pull(test2)
+    assert (np.min(a) == 2)
+    assert (np.max(a) == 6)
+    assert (np.mean(a) == 4)
