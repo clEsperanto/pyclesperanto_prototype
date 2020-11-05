5400b0d2545866dad915280c15050d46065be593
Robert Haase <haesleinhuepf@users.noreply.github.com>
2020-11-05 22:12:52 +0100

    fixed naming conventions of cl files and kernels

--- pyclesperanto_prototype/_tier1/_binary_edge_detection.py
+++ pyclesperanto_prototype/_tier1/_binary_edge_detection.py
@@ -24,5 +24,4 @@ def binary_edge_detection (src, dst):
         "src":src
     }
 
-    # TODO: Rename cl file and kernel function to fit to naming conventions. This needs to be done in clij2 as well.
-    execute(__file__, 'binaryEdgeDetection' + str(len(dst.shape)) + 'd_x.cl', 'binary_edge_detection_diamond_image' + str(len(dst.shape)) + 'd', dst.shape, parameters)
+    execute(__file__, 'binary_edge_detection_' + str(len(dst.shape)) + 'd_x.cl', 'binary_edge_detection_' + str(len(dst.shape)) + 'd', dst.shape, parameters)
