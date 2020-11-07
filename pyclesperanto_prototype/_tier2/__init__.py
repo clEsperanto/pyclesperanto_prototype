a3706567fb8dac2ce5cd6314007a024bc6224373
Robert Haase <haesleinhuepf@users.noreply.github.com>
2020-11-07 11:26:31 +0100

    added subtract_images

--- pyclesperanto_prototype/_tier2/__init__.py
+++ pyclesperanto_prototype/_tier2/__init__.py
@@ -3,6 +3,7 @@ from ._flag_existing_intensities import flag_existing_intensities
 from ._maximum_of_all_pixels import maximum_of_all_pixels
 from ._minimum_of_all_pixels import minimum_of_all_pixels
 from ._minimum_of_masked_pixels import minimum_of_masked_pixels
+from ._subtract_images import subtract_images
 from ._sum_of_all_pixels import sum_of_all_pixels
 from ._sum_reduction_x import sum_reduction_x
 from ._top_hat_sphere import top_hat_sphere
