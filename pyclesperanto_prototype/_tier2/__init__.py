75e65e6c1d904cd241a6c0b35227cb9f6f76ca5b
Robert Haase <haesleinhuepf@users.noreply.github.com>
2020-11-07 11:46:05 +0100

    added top_hat_box and tests for box and sphere

--- pyclesperanto_prototype/_tier2/__init__.py
+++ pyclesperanto_prototype/_tier2/__init__.py
@@ -6,4 +6,5 @@ from ._minimum_of_masked_pixels import minimum_of_masked_pixels
 from ._subtract_images import subtract_images
 from ._sum_of_all_pixels import sum_of_all_pixels
 from ._sum_reduction_x import sum_reduction_x
+from ._top_hat_box import top_hat_box
 from ._top_hat_sphere import top_hat_sphere
---
+++ tests/test_top_hat_sphere.py
@@ -0,0 +1,22 @@
+import pyclesperanto_prototype as cle
+import numpy as np
+
+def test_top_hat_sphere():
+
+    test = cle.push(np.asarray([
+        [0, 0, 0, 0, 0],
+        [0, 50, 50, 50, 0],
+        [0, 50, 100, 50, 0],
+        [0, 50, 50, 50, 0],
+        [0, 0, 0, 0, 0]
+    ]))
+
+    result = cle.create(test)
+    cle.top_hat_sphere(test, result, 1, 1, 0)
+
+    print(result)
+
+    a = cle.pull(result)
+    assert (np.min(a) == 0)
+    assert (np.max(a) == 50)
+
