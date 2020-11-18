75a9e58a40391934d9bb538888118591e25d68e4
Robert Haase <haesleinhuepf@users.noreply.github.com>
2020-11-18 09:57:25 +0100

    be more precise in comments and return types

--- pyclesperanto_prototype/_tier0/_available_device_names.py
+++ pyclesperanto_prototype/_tier0/_available_device_names.py
@@ -1,10 +1,12 @@
 from .._tier0._pycl import filter_devices
-def available_device_names():
-    """Retrieve a list of available OpenCL-devices
+from typing import List
+
+def available_device_names() -> List[str]:
+    """Retrieve a list of names of available OpenCL-devices
 
     Returns
     -------
-        list of OpenCL-Devices
+        list of OpenCL-device names
     """
     devices = filter_devices()
     device_names = [device.name for device in devices]
