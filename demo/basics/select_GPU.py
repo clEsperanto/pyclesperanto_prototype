228134c4529bd7e8687c98730df83fc0552edd9d
Robert Haase <haesleinhuepf@users.noreply.github.com>
2020-11-18 09:52:37 +0100

    added demo example for available_device_names

--- demo/basics/select_GPU.py
+++ demo/basics/select_GPU.py
@@ -1,5 +1,8 @@
 import pyclesperanto_prototype as cle
 
+# list names of all available OpenCL-devices
+print("Available OpenCL devices:" + str(cle.available_device_names()))
+
 # selecting an Nvidia RTX
 cle.select_device("RTX")
 print("Using OpenCL device " + cle.get_device().name)
@@ -19,3 +22,4 @@ print("Using OpenCL device " + cle.get_device().name)
 # selecting an AMD Vega GPU
 cle.select_device("902")
 print("Using OpenCL device " + cle.get_device().name)
+
