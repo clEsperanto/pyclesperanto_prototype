44b73497ac19289c0c9deb5124d0bc22e545c59a
Robert Haase <haesleinhuepf@users.noreply.github.com>
2020-11-18 15:06:54 +0100

    allow to filter devices before retrieving their names

--- demo/basics/select_GPU.py
+++ demo/basics/select_GPU.py
@@ -3,6 +3,13 @@ import pyclesperanto_prototype as cle
 # list names of all available OpenCL-devices
 print("Available OpenCL devices:" + str(cle.available_device_names()))
 
+# list CPUs and GPUs separately
+gpu_devices = cle.available_device_names(dev_type="gpu")
+print("Available GPU OpenCL devices:" + str(gpu_devices))
+
+cpu_devices = cle.available_device_names(dev_type="cpu")
+print("Available CPU OpenCL devices:" + str(cpu_devices))
+
 # selecting an Nvidia RTX
 cle.select_device("RTX")
 print("Using OpenCL device " + cle.get_device().name)
--- pyclesperanto_prototype/_tier0/_available_device_names.py
+++ pyclesperanto_prototype/_tier0/_available_device_names.py
@@ -1,13 +1,30 @@
 from .._tier0._pycl import filter_devices
 from typing import List
 
-def available_device_names() -> List[str]:
+def available_device_names(*args, **kwargs) -> List[str]:
     """Retrieve a list of names of available OpenCL-devices
+
+    Arguments are forwarded to `filter_devices`.
 
     Returns
     -------
         list of OpenCL-device names
+
+    See Also
+    --------
+    filter_devices : Returns list of devices instead of device names
+
+    Examples
+    --------
+    >>> import pyclesperanto_prototype as cle
+    >>> gpu_devices = cle.available_device_names(dev_type="gpu")
+    >>> print("Available GPU OpenCL devices:" + str(gpu_devices))
+    >>>
+    >>> cpu_devices = cle.available_device_names(dev_type="cpu")
+    >>> print("Available CPU OpenCL devices:" + str(cpu_devices))
+
+
     """
-    devices = filter_devices()
+    devices = filter_devices(*args, **kwargs)
     device_names = [device.name for device in devices]
     return device_names
