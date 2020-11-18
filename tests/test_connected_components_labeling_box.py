e01dc2e90d2b2e4e485b680365b92e4cf81533f4
Robert Haase <haesleinhuepf@users.noreply.github.com>
2020-11-18 10:21:52 +0100

    added complex real-use-case tests for connected_components_labeling_box and exclude_labels_on_edges

--- tests/test_connected_components_labeling_box.py
+++ tests/test_connected_components_labeling_box.py
@@ -28,3 +28,34 @@ def test_connected_components_labeling_box():
     print(b)
 
     assert (np.array_equal(a, b))
+
+def test_connected_components_labeling_box_blobs():
+    import pyclesperanto_prototype as cle
+
+    from skimage.io import imread, imsave
+
+    # initialize GPU
+    cle.select_device("GTX")
+    print("Used GPU: " + cle.get_device().name)
+
+    # load data
+    image = imread('https://imagej.nih.gov/ij/images/blobs.gif')
+    print("Loaded image size: " + str(image.shape))
+
+    # push image to GPU memory
+    input = cle.push(image)
+    print("Image size in GPU: " + str(input.shape))
+
+    # process the image
+    inverted = cle.subtract_image_from_scalar(image, scalar=255)
+    blurred = cle.gaussian_blur(inverted, sigma_x=1, sigma_y=1)
+    binary = cle.threshold_otsu(blurred)
+    labeled = cle.connected_components_labeling_box(binary)
+
+    # The maxmium intensity in a label image corresponds to the number of objects
+    num_labels = cle.maximum_of_all_pixels(labeled)
+
+    # print out result
+    print("Num objects in the image: " + str(num_labels))
+
+    assert num_labels == 63
