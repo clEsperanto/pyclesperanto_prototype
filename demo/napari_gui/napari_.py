3e891a9ab94882e88767d288a07353a51387956b
haesleinhuepf <robert_haase@gmx.de>
2020-11-25 23:21:19 +0100

    also add a minimal napari example

---
+++ demo/napari_gui/napari_.py
@@ -0,0 +1,26 @@
+# load data
+from skimage.io import imread
+image = imread('https://samples.fiji.sc/blobs.png')
+
+# start up napari
+import napari
+import pyclesperanto_prototype as cle
+with napari.gui_qt():
+    viewer = napari.Viewer()
+    viewer.add_image(image, name='blobs')
+
+    # push image to GPU
+    input = cle.push_zyx(image)
+
+    # process the mage
+    sigma = 3
+    blurred = cle.gaussian_blur(input, sigma_x=sigma, sigma_y=sigma)
+    binary = cle.threshold_otsu(blurred)
+    labels = cle.connected_components_labeling_box(binary)
+
+    # pull result back
+    output = cle.pull_zyx(labels)
+
+    # add it to napari
+    viewer.add_labels(output)
+
