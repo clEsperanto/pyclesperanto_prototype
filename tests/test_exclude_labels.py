4b46a3e3aa210e8582c64bf54f86688761b3e643
haesleinhuepf <robert_haase@gmx.de>
2020-12-12 10:27:14 +0100

    corrected names

--- tests/test_exclude_labels.py
+++ tests/test_exclude_labels.py
@@ -1,7 +1,7 @@
 import pyclesperanto_prototype as cle
 import numpy as np
 
-def test_exclude_labels_on_edges_2d():
+def test_exclude_labels_2d():
     
     gpu_input = cle.push(np.asarray([
 
@@ -38,7 +38,7 @@ def test_exclude_labels_on_edges_2d():
     assert (np.array_equal(a, b))
 
 
-def test_exclude_labels_on_edges_3d():
+def test_exclude_labels_3d():
     gpu_input = cle.push(np.asarray([
         [
             [0, 0, 2, 0, 0, 0, 0],
