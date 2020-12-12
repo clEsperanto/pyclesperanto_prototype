55cf151eef698c6e692b8608448212664722033a
haesleinhuepf <robert_haase@gmx.de>
2020-12-12 19:22:10 +0100

    added extend_labels_with_maximum_radius

--- pyclesperanto_prototype/_tier4/__init__.py
+++ pyclesperanto_prototype/_tier4/__init__.py
@@ -1,5 +1,6 @@
 from ._connected_components_labeling_box import connected_components_labeling_box
 from ._extend_labeling_via_voronoi import extend_labeling_via_voronoi
+from ._extend_labels_with_maximum_radius import extend_labels_wth_maximum_radius
 from ._mean_squared_error import mean_squared_error
 from ._sorensen_dice_coefficient import sorensen_dice_coefficient
 from ._spots_to_pointlist import spots_to_pointlist
