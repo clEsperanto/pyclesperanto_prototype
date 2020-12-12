0bfc16f25b45dce6fadffdceb9bbbfed00776b55
haesleinhuepf <robert_haase@gmx.de>
2020-12-12 23:09:38 +0100

    bugfix: ignore number of neighbors touching background

--- pyclesperanto_prototype/_tier4/_touching_neighbor_count_map.py
+++ pyclesperanto_prototype/_tier4/_touching_neighbor_count_map.py
@@ -17,7 +17,12 @@ def touching_neighbor_count_map(labels : Image, map_destination : Image = None):
     -------
 
     """
+    from .._tier1 import set_column
+
     touch_matrix = generate_touch_matrix(labels)
     number_of_touching_neighbors_vector = count_touching_neighbors(touch_matrix)
+
+    # ignore how many objects touch the background
+    set_column(number_of_touching_neighbors_vector, 0, 0)
     replace_intensities(labels, number_of_touching_neighbors_vector, map_destination)
     return map_destination
