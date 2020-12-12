4eae7bba4910c9443ea484985bd669084fe5d311
haesleinhuepf <robert_haase@gmx.de>
2020-12-12 13:36:30 +0100

    added mean_of_touching_neighbors

--- pyclesperanto_prototype/_tier2/__init__.py
+++ pyclesperanto_prototype/_tier2/__init__.py
@@ -5,6 +5,7 @@ from ._flag_existing_intensities import flag_existing_intensities
 from ._invert import invert
 from ._label_spots import label_spots
 from ._maximum_of_all_pixels import maximum_of_all_pixels
+from ._mean_of_touching_neighbors import mean_of_touching_neighbors
 from ._minimum_of_all_pixels import minimum_of_all_pixels
 from ._minimum_of_masked_pixels import minimum_of_masked_pixels
 from ._pointlist_to_labelled_spots import pointlist_to_labelled_spots
