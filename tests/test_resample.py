16923f40a1cea808db4e7ffdf8440036e21c3c49
haesleinhuepf <robert_haase@gmx.de>
2020-12-13 23:33:40 +0100

    ignore test using clImages in CI

--- tests/test_resample.py
+++ tests/test_resample.py
@@ -132,6 +132,12 @@ def test_resample_upsample_3d():
     assert (np.array_equal(a, b))
 
 
+import pytest
+import pyopencl as cl
+
+from . import LINUX, CI
+
+@pytest.mark.xfail('LINUX and CI', reason='clImages not supported on CI', raises=cl.LogicError)
 def test_resample_upsample_3d_with_interpolation():
     test1 = cle.push_zyx(np.asarray([
         [
