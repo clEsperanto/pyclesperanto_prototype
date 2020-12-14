a6b0dd002d70b605ce14c9f2937002192622ff81
haesleinhuepf <robert_haase@gmx.de>
2020-12-14 20:45:22 +0100

    bugfix: divide_by_gaussian_background didn't divide

--- pyclesperanto_prototype/_tier3/_divide_by_gaussian_background.py
+++ pyclesperanto_prototype/_tier3/_divide_by_gaussian_background.py
@@ -2,7 +2,7 @@ from pyclesperanto_prototype._tier0 import Image
 from pyclesperanto_prototype._tier1 import gaussian_blur
 from pyclesperanto_prototype._tier0 import create_like
 from pyclesperanto_prototype._tier0 import plugin_function
-from pyclesperanto_prototype._tier2 import subtract_images
+from pyclesperanto_prototype._tier1 import divide_images
 
 @plugin_function
 def divide_by_gaussian_background(input : Image, destination : Image = None, sigma_x : float = 2, sigma_y : float = 2, sigma_z : float = 2):
@@ -29,5 +29,5 @@ def divide_by_gaussian_background(input : Image, destination : Image = None, sig
 
     gaussian_blur(input, temp1, sigma_x, sigma_y, sigma_z)
 
-    return subtract_images(input, temp1, destination)
+    return divide_images(input, temp1, destination)
 
