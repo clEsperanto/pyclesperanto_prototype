dfd2e375f31bebcd01fba7f21a588072fc49730e
haesleinhuepf <robert_haase@gmx.de>
2020-12-26 11:25:40 +0100

    added centroids_of_background_and_labels

---
+++ pyclesperanto_prototype/_tier9/_centroids_of_background_and_labels.py
@@ -0,0 +1,13 @@
+from .._tier2 import maximum_of_all_pixels
+from .._tier0 import create_pointlist_from_labelmap
+from .._tier0 import execute
+from .._tier0 import plugin_function
+from .._tier0 import Image
+from .._tier0 import push
+
+@plugin_function(output_creator=create_pointlist_from_labelmap)
+def centroids_of_background_and_labels(source:Image, pointlist_destination :Image = None):
+    """todo
+    """
+    from .._tier9 import centroids_of_labels
+    return centroids_of_labels(source, pointlist_destination, True)
--- pyclesperanto_prototype/_tier9/_centroids_of_labels.py
+++ pyclesperanto_prototype/_tier9/_centroids_of_labels.py
@@ -6,7 +6,7 @@ from .._tier0 import Image
 from .._tier0 import push
 
 @plugin_function(output_creator=create_pointlist_from_labelmap)
-def centroids_of_labels(source:Image, pointlist_destination :Image = None):
+def centroids_of_labels(source:Image, pointlist_destination :Image = None, include_background :bool = False):
     """Determines the centroids of all labels in a label image or image stack. 
     
     It writes the resulting  coordinates in a pointlist image. Depending on 
@@ -27,23 +27,32 @@ def centroids_of_labels(source:Image, pointlist_destination :Image = None):
     .. [1] https://clij.github.io/clij2-docs/reference_centroidsOfLabels
     """
     from .._tier9 import statistics_of_labelled_pixels
-    regionprops = statistics_of_labelled_pixels(input=None, labelmap=source)
+    from .._tier9 import statistics_of_background_and_labelled_pixels
+    from .._tier2 import maximum_of_all_pixels
+
+    if include_background:
+        regionprops = statistics_of_background_and_labelled_pixels(input=None, labelmap=source)
+        num_rows = int(maximum_of_all_pixels(source) + 1)
+    else:
+        regionprops = statistics_of_labelled_pixels(input=None, labelmap=source)
+        num_rows = int(maximum_of_all_pixels(source))
 
     num_columns = len(source.shape)
-    num_rows = len(regionprops)
+    #num_rows = len(regionprops)
 
     import numpy as np
     matrix = np.zeros([num_rows, num_columns])
 
     for i, label_props in enumerate(regionprops):
-        #print(i)
+        index = label_props.label - 1
+        print(index)
         # centroid
         if (len(label_props.centroid) == 3):
-            matrix[i][0] = label_props.centroid[2]
-            matrix[i][1] = label_props.centroid[1]
-            matrix[i][2] = label_props.centroid[0]
+            matrix[index][0] = label_props.centroid[2]
+            matrix[index][1] = label_props.centroid[1]
+            matrix[index][2] = label_props.centroid[0]
         else:
-            matrix[i][0] = label_props.centroid[1]
-            matrix[i][1] = label_props.centroid[0]
+            matrix[index][0] = label_props.centroid[1]
+            matrix[index][1] = label_props.centroid[0]
 
     return push(matrix)
