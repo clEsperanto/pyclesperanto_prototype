7f8d573073f2d59019f6cea12224e07af3dcae7c
haesleinhuepf <robert_haase@gmx.de>
2021-01-03 15:55:04 +0100

    make push_regionprops_column actually push something, make it work with dictionaries delivered by the gpu-version of statistics_of_labelled_pixels

--- pyclesperanto_prototype/_tier9/_push_regionprops_column.py
+++ pyclesperanto_prototype/_tier9/_push_regionprops_column.py
@@ -1,16 +1,24 @@
-def push_regionprops_column(regionprops : list, column : str):
-    import numpy as np
-    if hasattr(regionprops[0], 'original_label'):
-        labels = [r.original_label for r in regionprops]
-    else:
-        labels = [r.label for r in regionprops]
-    max_label = np.max(labels)
+from typing import Union
 
-    values = np.zeros([max_label + 1])
-    for r in regionprops:
-        if hasattr(r, 'original_label'):
-            label = r.original_label
+
+def push_regionprops_column(regionprops : Union[list, dict], column : str):
+    from .._tier0 import push_zyx
+    import numpy as np
+
+    if isinstance(regionprops, dict):
+        return push_zyx(np.asarray(regionprops[column]))
+    else:
+        if hasattr(regionprops[0], 'original_label'):
+            labels = [r.original_label for r in regionprops]
         else:
-            label = r.label
-        values[label] = r[column]
-    return values
+            labels = [r.label for r in regionprops]
+        max_label = np.max(labels)
+
+        values = np.zeros([max_label + 1])
+        for r in regionprops:
+            if hasattr(r, 'original_label'):
+                label = r.original_label
+            else:
+                label = r.label
+            values[label] = r[column]
+        return push_zyx(np.asarray([values]))
--- tests/test_push_regionprops_column.py
+++ tests/test_push_regionprops_column.py
@@ -1,7 +1,7 @@
 import pyclesperanto_prototype as cle
 import numpy as np
 
-def test_statistics_of_background_and_labelled_pixels2():
+def test_push_regionprops_column():
     labels = cle.push_zyx(np.asarray([
         [
             [4, 4, 2],
@@ -13,7 +13,28 @@ def test_statistics_of_background_and_labelled_pixels2():
     ]))
     reference = [0, 1, 2, 2, 4]
 
-    regionprops = cle.statistics_of_background_and_labelled_pixels(None, labels)
+    regionprops = cle.statistics_of_background_and_labelled_pixels(None, labels, use_gpu=False)
+    areas = cle.push_regionprops_column(regionprops, 'area')
+
+    print(reference)
+    print(areas)
+
+    assert np.allclose(reference, areas, 0.001)
+
+
+def test_push_regionprops_column_gpu():
+    labels = cle.push_zyx(np.asarray([
+        [
+            [4, 4, 2],
+        ], [
+            [4, 1, 2],
+        ], [
+            [3, 3, 4]
+        ]
+    ]))
+    reference = [0, 1, 2, 2, 4]
+
+    regionprops = cle.statistics_of_background_and_labelled_pixels(None, labels, use_gpu=True)
     areas = cle.push_regionprops_column(regionprops, 'area')
 
     print(reference)
