75d035670f5368052edc36feb32a6ca8ae81bada
haesleinhuepf <robert_haase@gmx.de>
2021-01-09 12:53:30 +0100

    added test for point_index_list_to_mesh and fixed bug in cl-kernel

---
+++ tests/test_point_index_list_to_mesh.py
@@ -0,0 +1,35 @@
+import pyclesperanto_prototype as cle
+import numpy as np
+
+
+def test_point_index_list_to_mesh():
+    positions = cle.push_zyx(np.asarray([
+        [1, 1, 4, 4],
+        [1, 4, 4, 1]
+    ]))
+
+    index_list = cle.push_zyx(np.asarray([
+        [2, 3, 4, 1],
+        [3,-1,-1,-1]
+    ]))
+
+    reference = cle.push_zyx(np.asarray([
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 0],
+        [0, 1, 1, 0, 1, 0],
+        [0, 1, 0, 1, 1, 0],
+        [0, 1, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]))
+
+    result = cle.create_like(reference)
+
+    result = cle.point_index_list_to_mesh(positions, index_list, result)
+
+    a = cle.pull_zyx(result)
+    b = cle.pull_zyx(reference)
+
+    print(a)
+    print(b)
+
+    assert (np.array_equal(a, b))
