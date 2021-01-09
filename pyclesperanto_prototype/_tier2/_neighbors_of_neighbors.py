3b6676c262ceb078690506110ba993b48044b469
haesleinhuepf <robert_haase@gmx.de>
2021-01-09 15:30:44 +0100

    bugfix: use adjacency matrix instead of touch_matrix to determine neighbors of neighbors

--- pyclesperanto_prototype/_tier2/_neighbors_of_neighbors.py
+++ pyclesperanto_prototype/_tier2/_neighbors_of_neighbors.py
@@ -6,7 +6,7 @@ from .._tier1 import greater_constant
 from .._tier0 import create_like
 
 @plugin_function
-def neighbors_of_neighbors(touch_matrix : Image, neighbor_matrix_destination : Image = None, include_neighbors : bool = True):
+def neighbors_of_neighbors(touch_matrix : Image, neighbor_matrix_destination : Image = None):
     """Determines neighbors of neigbors from touch matrix and saves the result 
     as a new touch matrix. 
     
@@ -14,9 +14,7 @@ def neighbors_of_neighbors(touch_matrix : Image, neighbor_matrix_destination : I
     ----------
     touch_matrix : Image
     neighbor_matrix_destination : Image
-    include_neighbors : bool
-        Defines wheather current neigbors should be include in the resulting neighborhood
-    
+
     Returns
     -------
     neighbor_matrix_destination
@@ -25,14 +23,13 @@ def neighbors_of_neighbors(touch_matrix : Image, neighbor_matrix_destination : I
     ----------
     .. [1] https://clij.github.io/clij2-docs/reference_neighborsOfNeighbors
     """
+    from .._tier2 import touch_matrix_to_adjacency_matrix
+
+    touch_matrix = touch_matrix_to_adjacency_matrix(touch_matrix)
 
     temp = create_like(touch_matrix)
     multiply_matrix(touch_matrix, touch_matrix, temp)
 
-    if include_neighbors:
-        from .._tier1 import binary_or
-        neighbor_matrix_destination = binary_or(touch_matrix, temp, neighbor_matrix_destination)
-    else:
-        neighbor_matrix_destination = greater_constant(temp, neighbor_matrix_destination, 0)
+    neighbor_matrix_destination = greater_constant(temp, neighbor_matrix_destination, 0)
 
     return neighbor_matrix_destination
