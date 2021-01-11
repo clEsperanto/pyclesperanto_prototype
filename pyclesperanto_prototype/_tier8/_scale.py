1f03e17620e77bd5f790918310738d8cc92240a8
haesleinhuepf <robert_haase@gmx.de>
2021-01-11 10:09:10 +0100

    added zoom alias for scale + interoperability test with scipy

--- pyclesperanto_prototype/_tier8/_scale.py
+++ pyclesperanto_prototype/_tier8/_scale.py
@@ -1,8 +1,9 @@
 from .._tier0 import plugin_function
 from .._tier0 import Image
+from .._tier0 import create_none
 
-@plugin_function
-def scale(source : Image, destination : Image = None, factor_x : float = 1, factor_y : float = 1, factor_z : float = 1, centered=True):
+@plugin_function(output_creator=create_none)
+def scale(source : Image, destination : Image = None, factor_x : float = 1, factor_y : float = 1, factor_z : float = 1, centered=True, zoom = None):
     """Scale the image by given factors.
 
     Parameters
@@ -17,7 +18,13 @@ def scale(source : Image, destination : Image = None, factor_x : float = 1, fact
         scaling along y
     factor_z : float
         scaling along z
-    centered
+    centered : bool
+        if True the image is scaled towards its center (default)
+        if False it will be scaled towards the origin
+    zoom : float or sequence
+        The zoom factor along the axes (ignoring centering). If a float, zoom is the
+        same for each axis. If a sequence, zoom should contain
+        one value for each axis.
 
     Returns
     -------
@@ -26,6 +33,37 @@ def scale(source : Image, destination : Image = None, factor_x : float = 1, fact
     """
     from ._AffineTransform3D import AffineTransform3D
     from ._affine_transform import affine_transform
+    from .._tier0 import create
+
+    if destination is None:
+        import numpy as np
+        dimensions = np.asarray(source.shape)
+
+        # Todo: I'm sure the following block could be shorter.
+        if len(dimensions) == 3:
+            dimensions[0] = dimensions[0] * factor_z
+            dimensions[1] = dimensions[1] * factor_y
+            dimensions[2] = dimensions[2] * factor_z
+            if zoom is not None:
+                if isinstance(zoom, float) or isinstance(zoom, int):
+                    dimensions[0] = dimensions[0] * zoom
+                    dimensions[1] = dimensions[1] * zoom
+                    dimensions[2] = dimensions[2] * zoom
+                else:
+                    dimensions[0] = dimensions[0] * zoom[0]
+                    dimensions[1] = dimensions[1] * zoom[1]
+                    dimensions[2] = dimensions[2] * zoom[2]
+        else: # 2D image
+            dimensions[0] = dimensions[0] * factor_y
+            dimensions[1] = dimensions[1] * factor_x
+            if zoom is not None:
+                if isinstance(zoom, float) or isinstance(zoom, int):
+                    dimensions[0] = dimensions[0] * zoom
+                    dimensions[1] = dimensions[1] * zoom
+                else:
+                    dimensions[0] = dimensions[0] * zoom[0]
+                    dimensions[1] = dimensions[1] * zoom[1]
+        destination = create(dimensions)
 
     transform = AffineTransform3D()
     if centered:
@@ -35,5 +73,18 @@ def scale(source : Image, destination : Image = None, factor_x : float = 1, fact
 
     if centered:
         transform.center(source.shape, undo=True)
+
+    if zoom is not None:
+        import numpy as np
+        #zoom = np.asarray(zoom, dtype=np.float32)
+        #zoom = 1 / zoom
+        if isinstance(zoom, float):
+            if len(source.shape) == 3:
+                transform.scale(zoom, zoom, zoom)
+            else: # 2D image
+                transform.scale(zoom, zoom)
+        else:
+            zoom.reverse()
+            transform.scale(*zoom)
 
     return affine_transform(source, destination, transform)
--- tests/test_scale.py
+++ tests/test_scale.py
@@ -57,3 +57,92 @@ def test_scale_not_centered():
 
     assert (np.array_equal(a, b))
 
+def test_scale_compare_with_scipy():
+    import numpy as np
+    source = np.asarray([
+        [
+          [0, 1, 0, 0],
+          [0, 1, 0, 0],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0],
+        ], [
+          [0, 1, 0, 0],
+          [0, 1, 0, 0],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0],
+        ], [
+          [0, 0, 0, 0],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0],
+        ], [
+          [0, 0, 0, 0],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0],
+        ]
+    ])
+
+    import scipy
+    import numpy as np
+
+    zoom = [1, 2, 1]
+    reference = scipy.ndimage.zoom(source, zoom=zoom)
+    result = cle.zoom(source, zoom=zoom)
+    assert np.allclose(result, reference, 0.001)
+
+    zoom = [1, 0.5, 1]
+    reference = scipy.ndimage.zoom(source, zoom=zoom)
+    result = cle.zoom(source, zoom=zoom)
+    assert np.allclose(result, reference, 0.001)
+
+    zoom = [2, 1, 1]
+    reference = scipy.ndimage.zoom(source, zoom=zoom)
+    result = cle.zoom(source, zoom=zoom)
+    assert np.allclose(result, reference, 0.001)
+
+    zoom = [0.5, 1, 1]
+    reference = scipy.ndimage.zoom(source, zoom=zoom)
+    result = cle.zoom(source, zoom=zoom)
+    assert np.allclose(result, reference, 0.001)
+
+    zoom = [1, 1, 2]
+    reference = scipy.ndimage.zoom(source, zoom=zoom)
+    result = cle.zoom(source, zoom=zoom)
+    assert np.allclose(result, reference, 0.001)
+
+# for this one, we need a different image to make scipy output something reasonable.
+# in practice, this should not matter
+def test_scale_compare_with_scipy_failing():
+    import numpy as np
+    source = np.asarray([
+        [
+            [1, 1, 0, 0],
+            [1, 1, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ], [
+            [1, 1, 0, 0],
+            [1, 1, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ], [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ], [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ]
+    ])
+
+    import scipy
+    import numpy as np
+
+    zoom = [1, 1, 0.5]
+    reference = scipy.ndimage.zoom(source, zoom=zoom)
+    result = cle.zoom(source, zoom=zoom)
+    assert np.allclose(result, reference, 0.001)
