b4f8e166f37998e7d14d8f1bb2baeb1d07152cbc
haesleinhuepf <robert_haase@gmx.de>
2021-03-21 19:17:21 +0100

    added tests for interpolation in X, Y and Z

--- tests/test_resample.py
+++ tests/test_resample.py
@@ -163,3 +163,70 @@ def test_resample_upsample_3d_with_interpolation():
     print(a)
     print(b)
     assert (np.array_equal(a, b))
+
+def test_resample_3d_interpolation_x():
+    test1 = cle.push(np.asarray([
+        [
+            [0, 2]
+        ]
+    ]))
+    reference = cle.push(np.asarray([
+        [
+            [0, 0.5, 1.5, 1.5],
+        ]
+    ]))
+
+
+    result = cle.resample(test1, factor_x=2, factor_y=1, factor_z=1,linear_interpolation=True)
+
+    a = cle.pull(result)
+    b = cle.pull(reference)
+
+    print(a)
+    print(b)
+    assert (np.array_equal(a, b))
+
+def test_resample_3d_interpolation_y():
+    test1 = cle.push(np.asarray([
+        [
+            [0], [2]
+        ]
+    ]))
+    reference = cle.push(np.asarray([
+        [
+            [0], [0.5], [1.5], [1.5],
+        ]
+    ]))
+
+
+    result = cle.resample(test1, factor_x=1, factor_y=2, factor_z=1,linear_interpolation=True)
+
+    a = cle.pull(result)
+    b = cle.pull(reference)
+
+    print(a)
+    print(b)
+    assert (np.array_equal(a, b))
+
+
+def test_resample_3d_interpolation_z():
+    test1 = cle.push(np.asarray([
+
+            [[0]], [[2]]
+
+    ]))
+    reference = cle.push(np.asarray([
+
+            [[0]], [[0.5]], [[1.5]], [[1.5]],
+
+    ]))
+
+    result = cle.resample(test1, factor_x=1, factor_y=1, factor_z=2, linear_interpolation=True)
+
+    a = cle.pull(result)
+    b = cle.pull(reference)
+
+    print(a)
+    print(b)
+    assert (np.array_equal(a, b))
+
