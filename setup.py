0b97a1b78d29985579cae97b83639f7ad9d00ed1
haesleinhuepf <robert_haase@gmx.de>
2021-04-22 08:16:07 +0200

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyclesperanto_prototype",
-    version="0.7.6",
+    version="0.8.0",
     author="haesleinhuepf",
     author_email="robert.haase@tu-dresden.de",
     description="OpenCL-based GPU-accelerated image processing",
