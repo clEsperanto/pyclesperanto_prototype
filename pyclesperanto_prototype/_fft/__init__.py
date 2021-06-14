93a1a7d3dbb6618a2cf8a8a42ab8f6162af53548
Talley Lambert <talley.lambert@gmail.com>
2021-06-14 11:49:06 -0400

    add aliases

--- pyclesperanto_prototype/_fft/__init__.py
+++ pyclesperanto_prototype/_fft/__init__.py
@@ -1,3 +1,19 @@
 from ._fft import fftn, ifftn
 from ._fftshift import fftshift
 from ._fftconvolve import fftconvolve
+
+# clij2 aliases
+convolve_fft = fftconvolve
+inversere_fft = ifftn
+forward_fft = fftn
+fft = fftn
+
+__all__ = [
+    "convolve_fft",
+    "fft",
+    "fftn",
+    "fftshift",
+    "forward_fft",
+    "ifftn",
+    "inversere_fft",
+]
