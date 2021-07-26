38f8a6efe30d3df306ebd6022007ed01728596c7
Robert Haase <haesleinhuepf@users.noreply.github.com>
2021-07-26 14:21:18 +0200

    remove open/close labels because it's not picture-perfect yet

--- pyclesperanto_prototype/_tier5/__init__.py
+++ pyclesperanto_prototype/_tier5/__init__.py
@@ -1,6 +1,4 @@
-from ._close_labels import close_labels
 from ._connected_components_labeling_diamond import connected_components_labeling_diamond
-from ._open_labels import open_labels
 from ._voronoi_labeling import voronoi_labeling
 from ._masked_voronoi_labeling import masked_voronoi_labeling
 
