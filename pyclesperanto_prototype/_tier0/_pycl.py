105eac0dfc96e5d24fe96fa584285a9f3e0d6a46
Robert Haase <haesleinhuepf@users.noreply.github.com>
2021-08-11 20:20:14 +0200

    Removed wrapper, renamed class

--- pyclesperanto_prototype/_tier0/_pycl.py
+++ pyclesperanto_prototype/_tier0/_pycl.py
@@ -102,7 +102,7 @@ def assert_bufs_type(mytype, *bufs):
 def prepare(arr):
     return np.require(arr, None, "C")
 
-class CLEArray(array.Array, np.lib.mixins.NDArrayOperatorsMixin):
+class OCLArray(array.Array, np.lib.mixins.NDArrayOperatorsMixin):
 
     def __init__(self, queue, shape, dtype, order="C", allocator=None,
                  data=None, offset=0, strides=None, events=None, _flags=None,
@@ -115,25 +115,25 @@ class CLEArray(array.Array, np.lib.mixins.NDArrayOperatorsMixin):
     def from_array(cls, arr, *args, **kwargs):
         assert_supported_ndarray_type(arr.dtype.type)
         queue = get_device().queue
-        return CLEArray.to_device(queue, prepare(arr), *args, **kwargs)
+        return OCLArray.to_device(queue, prepare(arr), *args, **kwargs)
 
     @classmethod
     def empty(cls, shape, dtype=np.float32):
         assert_supported_ndarray_type(dtype)
         queue = get_device().queue
-        return CLEArray(queue, shape, dtype)
+        return OCLArray(queue, shape, dtype)
 
     @classmethod
     def empty_like(cls, arr):
         assert_supported_ndarray_type(arr.dtype.type)
         queue = get_device().queue # todo: get queue from arr
-        return CLEArray(queue, arr.shape, arr.dtype.type)
+        return OCLArray(queue, arr.shape, arr.dtype.type)
 
     @classmethod
     def zeros(cls, shape, dtype=np.float32):
         assert_supported_ndarray_type(dtype)
         queue = get_device().queue
-        new_array = CLEArray(queue, shape, dtype)
+        new_array = OCLArray(queue, shape, dtype)
         from .._tier1 import set
         set(new_array, 0)
         return new_array
@@ -159,17 +159,17 @@ class CLEArray(array.Array, np.lib.mixins.NDArrayOperatorsMixin):
 
     @classmethod
     def to_device(cls, queue, ary, *args, **kwargs):
-        if isinstance(ary, CLEArray):
+        if isinstance(ary, OCLArray):
             return ary
-        cl_a = CLEArray(queue, ary.shape, ary.dtype, strides=ary.strides)
+        cl_a = OCLArray(queue, ary.shape, ary.dtype, strides=ary.strides)
         cl_a.set(ary, queue=queue)
         return cl_a
 
     def __array_ufunc__(self, ufunc, method, *inputs, **kwargs):
         if method == "__call__":
-            func = getattr(CLEArray, f'__{ufunc.__name__}__', None)
+            func = getattr(OCLArray, f'__{ufunc.__name__}__', None)
             if func is not None:
-                return func(*[CLEArray.to_device(self.queue, i) for i in inputs], **kwargs)
+                return func(*[OCLArray.to_device(self.queue, i) for i in inputs], **kwargs)
         return NotImplemented
 
     def copy_image(self, img, **kwargs):
@@ -377,354 +377,6 @@ class CLEArray(array.Array, np.lib.mixins.NDArrayOperatorsMixin):
             from .._tier1 import power_images
             return power_images(temp, x2, x1)
 
-
-
-
-
-
-def _wrap_OCLArray(cls):
-    """
-    WRAPPER
-    """
-
-
-
-    @classmethod
-    def from_array(cls, arr, *args, **kwargs):
-        assert_supported_ndarray_type(arr.dtype.type)
-        queue = get_device().queue
-        return array.to_device(queue, prepare(arr), *args, **kwargs)
-
-    @classmethod
-    def empty(cls, shape, dtype=np.float32):
-        assert_supported_ndarray_type(dtype)
-        queue = get_device().queue
-        return array.empty(queue, shape, dtype)
-
-    @classmethod
-    def empty_like(cls, arr):
-        assert_supported_ndarray_type(arr.dtype.type)
-        return cls.empty(arr.shape, arr.dtype.type)
-
-    @classmethod
-    def zeros(cls, shape, dtype=np.float32):
-        assert_supported_ndarray_type(dtype)
-        queue = get_device().queue
-        return array.zeros(queue, shape, dtype)
-
-    @classmethod
-    def zeros_like(cls, arr):
-        assert_supported_ndarray_type(arr.dtype.type)
-        queue = get_device().queue
-        return array.zeros(queue, arr.shape, arr.dtype.type)
-
-    def copy_buffer(self, buf, **kwargs):
-        queue = get_device().queue
-        return cl.enqueue_copy(queue, self.data, buf.data, **kwargs)
-
-    def write_array(self, arr, **kwargs):
-        assert_supported_ndarray_type(arr.dtype.type)
-        queue = get_device().queue
-        return cl.enqueue_copy(queue, self.data, prepare(arr), **kwargs)
-
-    def copy_image(self, img, **kwargs):
-        queue = get_device().queue
-        return cl.enqueue_copy(
-            queue,
-            self.data,
-            img,
-            offset=0,
-            origin=(0,) * len(img.shape),
-            region=img.shape,
-            **kwargs,
-        )
-
-    def wrap_module_func(mod, f):
-        def func(self, *args, **kwargs):
-            return getattr(mod, f)(self, *args, **kwargs)
-
-        return func
-
-    cls.from_array = from_array
-    cls.empty = empty
-    cls.empty_like = empty_like
-    cls.zeros = zeros
-    cls.zeros_like = zeros_like
-    cls.copy_buffer = copy_buffer
-    cls.copy_image = copy_image
-    cls.write_array = write_array
-
-    cls.__array__ = cls.get
-
-    def add(x1, x2):
-        if isinstance(x2, (int, float)) :
-            from .._tier1 import add_image_and_scalar
-            return add_image_and_scalar(x1, scalar=x2)
-        else:
-            from .._tier1 import add_images_weighted
-            return add_images_weighted(x1, x2)
-    cls.__add__ = add
-
-    def iadd(x1, x2):
-        from .._tier1 import copy
-        temp = copy(x1)
-        if isinstance(x2, (int, float)) :
-            from .._tier1 import add_image_and_scalar
-            return add_image_and_scalar(temp, x1, scalar=x2)
-        else:
-            from .._tier1 import add_images_weighted
-            return add_images_weighted(temp, x2, x1)
-    cls.__iadd__ = iadd
-
-    def sub(x1, x2):
-        if isinstance(x2, (int, float)):
-            from .._tier1 import add_image_and_scalar
-            return add_image_and_scalar(x1, scalar=-x2)
-        else:
-            from .._tier1 import add_images_weighted
-            return add_images_weighted(x1, x2, factor2=-1)
-    cls.__sub__ = sub
-
-    def isub(x1, x2):
-        from .._tier1 import copy
-        temp = copy(x1)
-        if isinstance(x2, (int, float)) :
-            from .._tier1 import add_image_and_scalar
-            return add_image_and_scalar(temp, x1, scalar=-x2)
-        else:
-            from .._tier1 import add_images_weighted
-            return add_images_weighted(temp, x2, x1, factor2=-1)
-    cls.__isub__ = isub
-
-    def mul(x1, x2):
-        if isinstance(x2, (int, float)):
-            from .._tier1 import multiply_image_and_scalar
-            return multiply_image_and_scalar(x1, scalar=x2)
-        else:
-            from .._tier1 import multiply_images
-            return multiply_images(x1, x2)
-    cls.__mul__ = mul
-
-    def imul(x1, x2):
-        from .._tier1 import copy
-        temp = copy(x1)
-        if isinstance(x2, (int, float)):
-            from .._tier1 import multiply_image_and_scalar
-            return multiply_image_and_scalar(temp, x1, scalar=x2)
-        else:
-            from .._tier1 import multiply_images
-            return multiply_images(temp, x2, x1)
-
-    cls.__imul__ = imul
-
-    def div(x1, x2):
-        if isinstance(x2, (int, float)):
-            from .._tier1 import multiply_image_and_scalar
-            return multiply_image_and_scalar(x1, scalar=1.0 / x2)
-        else:
-            from .._tier1 import divide_images
-            return divide_images(x1, x2)
-    cls.__div__ = div
-    cls.__truediv__ = div
-
-    def idiv(x1, x2):
-        from .._tier1 import copy
-        temp = copy(x1)
-        if isinstance(x2, (int, float)):
-            from .._tier1 import multiply_image_and_scalar
-            return multiply_image_and_scalar(temp, x1, scalar=1.0 / x2)
-        else:
-            from .._tier1 import divide_images
-            return divide_images(temp, x2, x1)
-    cls.__idiv__ = idiv
-    cls.__itruediv__ = idiv
-
-    def gt(x1, x2):
-        if isinstance(x2, (int, float)):
-            from .._tier1 import greater_constant
-            return greater_constant(x1, constant=x2)
-        else:
-            from .._tier1 import greater
-            return greater(x1, x2)
-    cls.__gt__ = gt
-
-    def ge(x1, x2):
-        if isinstance(x2, (int, float)):
-            from .._tier1 import greater_or_equal_constant
-            return greater_or_equal_constant(x1, constant=x2)
-        else:
-            from .._tier1 import greater_or_equal
-            return greater_or_equal(x1, x2)
-    cls.__ge__ = ge
-
-    def lt(x1, x2):
-        if isinstance(x2, (int, float)):
-            from .._tier1 import smaller_constant
-            return smaller_constant(x1, constant=x2)
-        else:
-            from .._tier1 import smaller
-            return smaller(x1, x2)
-    cls.__lt__ = lt
-
-    def le(x1, x2):
-        if isinstance(x2, (int, float)):
-            from .._tier1 import smaller_or_equal_constant
-            return smaller_or_equal_constant(x1, constant=x2)
-        else:
-            from .._tier1 import smaller_or_equal
-            return smaller_or_equal(x1, x2)
-    cls.__le__ = le
-
-    def eq(x1, x2):
-        if isinstance(x2, (int, float)):
-            from .._tier1 import equal_constant
-            return equal_constant(x1, constant=x2)
-        else:
-            from .._tier1 import equal
-            return equal(x1, x2)
-    cls.__eq__ = eq
-
-    def ne(x1, x2):
-        if isinstance(x2, (int, float)):
-            from .._tier1 import not_equal_constant
-            return not_equal_constant(x1, constant=x2)
-        else:
-            from .._tier1 import not_equal
-            return not_equal(x1, x2)
-    cls.__ne__ = ne
-
-    def pos(x1):
-        from .._tier1 import copy
-        return copy(x1)
-    cls.__pos__ = pos
-
-    def neg(x1):
-        from .._tier1 import subtract_image_from_scalar
-        return subtract_image_from_scalar(x1, scalar=0)
-    cls.__neg__ = neg
-
-    def pow(x1, x2):
-        if isinstance(x2, (int, float)):
-            from .._tier1 import power
-            return power(x1, exponent=x2)
-        else:
-            from .._tier1 import power_images
-            return power_images(x1, x2)
-    cls.__pow__ = pow
-
-    def ipow(x1, x2):
-        from .._tier1 import copy
-        temp = copy(x1)
-        if isinstance(x2, (int, float)):
-            from .._tier1 import power
-            return power(temp, x1, exponent=x2)
-        else:
-            from .._tier1 import power_images
-            return power_images(temp, x2, x1)
-    cls.__ipow__ = ipow
-
-    def min(self, axis=None, out=None):
-        from .._tier2 import minimum_of_all_pixels
-        from .._tier1 import minimum_x_projection
-        from .._tier1 import minimum_y_projection
-        from .._tier1 import minimum_z_projection
-
-        if axis==0:
-            result = minimum_z_projection(self)
-        elif axis==1:
-            result = minimum_y_projection(self)
-        elif axis==2:
-            result = minimum_x_projection(self)
-        elif axis is None:
-            result = minimum_of_all_pixels(self)
-        else:
-            raise ValueError("Axis " + axis + " not supported")
-        if out is not None:
-            np.copyto(out, result.get().astype(out.dtype))
-        return result
-    cls.min = min
-
-    def max(self, axis=None, out=None):
-        from .._tier2 import maximum_of_all_pixels
-        from .._tier1 import maximum_x_projection
-        from .._tier1 import maximum_y_projection
-        from .._tier1 import maximum_z_projection
-
-        if axis==0:
-            result = maximum_z_projection(self)
-        elif axis==1:
-            result = maximum_y_projection(self)
-        elif axis==2:
-            result = maximum_x_projection(self)
-        elif axis is None:
-            result = maximum_of_all_pixels(self)
-        else:
-            raise ValueError("Axis " + axis + " not supported")
-        if out is not None:
-            np.copyto(out, result.get().astype(out.dtype))
-        return result
-    cls.max = max
-
-    def sum(self, axis=None, out=None):
-        from .._tier2 import sum_of_all_pixels
-        from .._tier1 import sum_x_projection
-        from .._tier1 import sum_y_projection
-        from .._tier1 import sum_z_projection
-
-        if axis==0:
-            result = sum_z_projection(self)
-        elif axis==1:
-            result = sum_y_projection(self)
-        elif axis==2:
-            result = sum_x_projection(self)
-        elif axis is None:
-            result = sum_of_all_pixels(self)
-        else:
-            raise ValueError("Axis " + axis + " not supported")
-        if out is not None:
-            np.copyto(out, result.get().astype(out.dtype))
-        return result
-    cls.sum = sum
-
-    cls._former_get = cls._get
-    def _cust_get(self, queue=None, ary=None, async_=None, **kwargs):
-        if not isinstance(queue, cl.CommandQueue):
-            queue = None
-        return self._former_get(queue, ary, async_, **kwargs)
-    cls._get = _cust_get
-
-    cls._former_get_item = cls.__getitem__
-
-    def _cust_get_item(self, index):
-        try:
-            return self._former_get_item(index)
-        except IndexError:
-            raise IndexError("Accessing individual GPU-backed pixels is not fully supported. If you work in napari, use the menu Plugins > clEsperanto > Make labels editable. If you work in python, use numpy.asarray(image) to retrieve a fully accessible copy of the image.")
-    cls.__getitem__ = _cust_get_item
-
-    # todo:
-    #  __floordiv__(x1, x2)
-    #  __mod__(x1, x2)
-    #  __matmul__(x1, x2)
-    #  __inv__(x1, x2)
-    #  __invert__(x1, x2)
-    #  __lshift__(x1, x2)
-    #  __rshift__(x1, x2)
-    # and, or, xor
-
-    for f in ["dot", "vdot"]:
-        setattr(cls, f, wrap_module_func(array, f))
-
-    # for f in dir(cl_math):
-    #    if callable(getattr(cl.cl_math, f)):
-    #        setattr(cls, f, wrap_module_func(cl.cl_math, f))
-
-    # cls.sum = sum
-    cls.__name__ = str("OCLArray")
-    return cls
-
-
-OCLArray = CLEArray #_wrap_OCLArray(array.Array)
 
 class _OCLImage:
     def __init__(self, cl_image : cl.Image):
