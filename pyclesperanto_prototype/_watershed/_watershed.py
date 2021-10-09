e97ae8461e647f7c3974c93c9e4e385a1450ab60
Robert Haase <haesleinhuepf@users.noreply.github.com>
2021-10-09 22:12:56 +0200

    WIP

--- pyclesperanto_prototype/_watershed/_watershed.py
+++ pyclesperanto_prototype/_watershed/_watershed.py
@@ -1,16 +1,55 @@
 from .._tier0 import plugin_function
 from .._tier0 import Image
-from .._tier0 import execute, create, create_labels_like, create_like
-from .._tier1 import set, copy
+from .._tier0 import execute, create, create_labels_like, create_like, create_none
+from .._tier1 import set, copy, add_image_and_scalar
 
-@plugin_function(categories=['label processing', 'in assistant'])
-def watershed(image:Image, markers:Image, mask:Image,output:Image = None):
+@plugin_function(categories=['label processing', 'in assistant'], output_creator=create_none)
+def watershed(image:Image, markers:Image, mask:Image=None,output:Image = None):
+    """
+    Watershed algorithm takes seed labels (markers) and enlarges
+    their areas according to the landscape / altitude map provided as image.
+
+    The API of this algorithm is different from the implementation in CLIJ2 [1]
+
+    The results may be similar, but not identical to the results by the implementation in scikit-image[2].
+
+    Parameters
+    ----------
+    image : Image
+        landscape or altitude image
+    markers : Image
+        seeds
+    mask : Image, optional
+        limit label enlarging to this region
+    output : Image, optional
+        label image with resulting basins
+
+    Returns
+    -------
+    output
+
+    See Also
+    --------
+    [1] https://clij.github.io/clij2-docs/reference_watershed
+    [2] https://scikit-image.org/docs/dev/api/skimage.segmentation.html#skimage.segmentation.watershed
+
+    """
     flag = create([1, 1, 1])
     set(flag, 1)
+
+    if mask is None:
+        mask = create_like(image)
+        set(mask, 1)
+
+    if output is None:
+        output = create_labels_like(markers)
     set(output, 0)
 
-    distance_map1 = create_like(image)
-    copy(image, distance_map1)
+    temp = image * -1 # negate to make API similar to skimage
+    distance_map1 = add_image_and_scalar(temp, scalar=-temp.min()) # make sure it's positive
+    print(distance_map1.min())
+    print(distance_map1.max())
+
     distance_map2 = create_like(image)
     set(distance_map2, 0)
 
@@ -19,20 +58,20 @@ def watershed(image:Image, markers:Image, mask:Image,output:Image = None):
     label_map_2 = create_labels_like(markers)
     set(label_map_2, 0)
 
-    print("hello world")
-
     iteration = 0
+    # enlarge labels according to image gradient iteratively
     while(flag[0,0,0] > 0):
         set(flag, 0)
         if iteration % 2 == 0:
-            _dilate_labels_until_no_change(distance_map1, label_map_1, flag, distance_map2, label_map_2, mask)
+            _dilate_labels_until_no_change(distance_map1, label_map_1, flag, distance_map2, label_map_2, mask, temp)
         else:
-            _dilate_labels_until_no_change(distance_map2, label_map_2, flag, distance_map1, label_map_1, mask)
+            _dilate_labels_until_no_change(distance_map2, label_map_2, flag, distance_map1, label_map_1, mask, temp)
+
+        if iteration % 10 == 0:
+            from .._tier9 import imshow
+            imshow(distance_map1)
 
         iteration += 1
-
-        if iteration > 10:
-            break
 
     if iteration % 2 == 0:
         copy(label_map_1, output)
@@ -41,13 +80,14 @@ def watershed(image:Image, markers:Image, mask:Image,output:Image = None):
 
     return output
 
-def _dilate_labels_until_no_change(distanceMapIn:Image, labelMapIn:Image, flag:Image, distanceMapOut:Image, labelMapOut:Image, mask:Image):
+def _dilate_labels_until_no_change(distanceMapIn:Image, labelMapIn:Image, flag:Image, distanceMapOut:Image, labelMapOut:Image, mask:Image, original_distance_map:Image):
     parameters = {
     "src_labelmap": labelMapIn,
     "src_distancemap": distanceMapIn,
     "dst_labelmap": labelMapOut,
     "dst_distancemap": distanceMapOut,
     "flag_dst": flag,
-    "mask": mask}
+    "src_mask": mask,
+    "src_original_distance_map": original_distance_map}
 
     execute(__file__, "watershed_local_maximum_" + str(len(distanceMapIn.shape)) + "d_x.cl", "watershed_local_maximum_" + str(len(distanceMapIn.shape)) + "d", distanceMapIn.shape, parameters)
--- pyclesperanto_prototype/_watershed/watershed_local_maximum_2d_x.cl
+++ pyclesperanto_prototype/_watershed/watershed_local_maximum_2d_x.cl
@@ -5,7 +5,8 @@ __kernel void watershed_local_maximum_2d
   IMAGE_dst_labelmap_TYPE dst_labelmap,
   IMAGE_dst_distancemap_TYPE dst_distancemap,
   IMAGE_flag_dst_TYPE flag_dst,
-  IMAGE_mask_TYPE mask
+  IMAGE_src_mask_TYPE src_mask,
+  IMAGE_src_original_distance_map_TYPE src_original_distance_map
 )
 {
   const sampler_t sampler = CLK_NORMALIZED_COORDS_FALSE | CLK_ADDRESS_CLAMP_TO_EDGE | CLK_FILTER_NEAREST;
@@ -15,9 +16,10 @@ __kernel void watershed_local_maximum_2d
 
   const int2 pos = (int2){x,y};
 
-  float mask_value = READ_mask_IMAGE(mask, sampler, pos).x;
-  float currentlabel = READ_src_labelmap_IMAGE(src_labelmap, sampler, pos).x;
-  float currentdistance = READ_src_distancemap_IMAGE(src_distancemap, sampler, pos).x;
+  float mask_value = READ_IMAGE(src_mask, sampler, pos).x;
+  float currentlabel = READ_IMAGE(src_labelmap, sampler, pos).x;
+  float currentdistance = READ_IMAGE(src_distancemap, sampler, pos).x;
+  float originaldistance = READ_IMAGE(src_original_distance_map, sampler, pos).x;
 
   float bestlabel = currentlabel;
   float bestdistance = currentdistance;
@@ -25,14 +27,16 @@ __kernel void watershed_local_maximum_2d
   if (mask_value > 0 && currentlabel < 1) {
       for (int ax = -1; ax <= 1; ax++) {
         for (int ay = -1; ay <= 1; ay++) {
-          float distance = READ_src_distancemap_IMAGE(src_distancemap, sampler, (pos + (int2){ax, ay})).x;
+          float distance = READ_IMAGE(src_distancemap, sampler, (pos + (int2){ax, ay})).x;
+          float remote_distance = READ_IMAGE(src_original_distance_map, sampler, (pos + (int2){ax, ay})).x;
           //if (distance > 0) {
-            if (distance > bestdistance) {
-              float label = READ_src_labelmap_IMAGE(src_labelmap, sampler, (pos + (int2){ax, ay})).x;
+          if (distance > bestdistance) {
+            //if (distance > bestdistance) {
+              float label = READ_IMAGE(src_labelmap, sampler, (pos + (int2){ax, ay})).x;
               bestdistance = distance;
               bestlabel = label;
-            }
-          //}
+            //}
+          }
         }
       }
 
