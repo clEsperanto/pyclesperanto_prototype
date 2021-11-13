45c10706cd015c36e36f0140e841f67ecad734bb
Robert Haase <haesleinhuepf@users.noreply.github.com>
2021-11-13 21:06:38 +0100

    bugfix: too many axis transpositions

--- pyclesperanto_prototype/_tier0/_cuda_backend.py
+++ pyclesperanto_prototype/_tier0/_cuda_backend.py
@@ -43,7 +43,7 @@ class CUDAArray(ArrayOperators, np.lib.mixins.NDArrayOperatorsMixin):
         return self.array.ndims
 
     def get(self, *args, **kwargs):
-        return self.array.T.get(*args, **kwargs)
+        return self.array.get(*args, **kwargs)
 
     def __array__(self, dtype=None):
         if dtype is None:
