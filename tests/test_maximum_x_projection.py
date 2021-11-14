1447f68a99fd2377746ec49d8b86d65d37dca2ef
Robert Haase <haesleinhuepf@users.noreply.github.com>
2021-11-14 16:29:48 +0100

    updated maximum-?-projection tests to not use .T anymore

--- tests/test_maximum_x_projection.py
+++ tests/test_maximum_x_projection.py
@@ -35,15 +35,15 @@ def test_maximum_x_projection():
             [0, 2, 0, 8, 0],
             [5, 0, 6, 0, 10]
         ]
-    ]).T)
+    ]))
 
     reference = cle.push(np.asarray([
-        [1, 3, 3, 3, 5],
-        [2, 4, 4, 4, 0],
-        [0, 1, 1, 1, 6],
-        [8, 8, 7, 8, 0],
-        [9, 10, 10, 10, 10]
-    ]).T)
+        [9, 8, 8, 8, 9],
+        [8, 9, 10, 9, 7],
+        [10, 10, 7, 7, 10],
+        [7, 7, 9, 10, 8],
+        [10, 10, 10, 10, 10]
+    ]))
 
     result = cle.create(reference)
     cle.maximum_x_projection(test1, result)
