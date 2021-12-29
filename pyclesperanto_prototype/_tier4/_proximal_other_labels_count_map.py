26074c6a07fab39956a2de9eb4b2aa264d28a9fc
Robert Haase <haesleinhuepf@users.noreply.github.com>
2021-12-29 19:55:20 +0100

    added proximal_other_labels_count_map properly to the assistant

--- pyclesperanto_prototype/_tier4/_proximal_other_labels_count_map.py
+++ pyclesperanto_prototype/_tier4/_proximal_other_labels_count_map.py
@@ -1,8 +1,8 @@
 from .._tier0 import plugin_function, Image, create_none
 import numpy as np
 
-@plugin_function(categories=['label measurement', 'combine', 'map', 'label comparison'], output_creator=create_none)
-def proximal_other_labels_count_map(label_image:Image, other_label_image:Image, count_map:Image = None, maximum_distance=25):
+@plugin_function(categories=['label measurement', 'combine', 'map', 'label comparison', 'in assistant'], output_creator=create_none)
+def proximal_other_labels_count_map(label_image:Image, other_label_image:Image, count_map:Image = None, maximum_distance: float = 25):
     """
     Count number of labels within a given radius in an other label image and returns the result as parametric map.
 
