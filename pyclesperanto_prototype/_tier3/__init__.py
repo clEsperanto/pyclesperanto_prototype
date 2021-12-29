e65443331968d3fe1f9ebc291817e91c6197d672
Robert Haase <haesleinhuepf@users.noreply.github.com>
2021-12-29 14:58:13 +0100

    added proximal_other_labels_count + map + tests + demo notebook

--- pyclesperanto_prototype/_tier3/__init__.py
+++ pyclesperanto_prototype/_tier3/__init__.py
@@ -1,6 +1,7 @@
 from ._absolute_difference import absolute_difference
 from ._bounding_box import bounding_box
 from ._center_of_mass import center_of_mass
+from ._proximal_other_labels_count import proximal_other_labels_count
 from ._relabel_sequential import relabel_sequential
 from ._relabel_sequential import relabel_sequential as close_index_gaps_in_label_map
 from ._difference_of_gaussian import difference_of_gaussian
---
+++ pyclesperanto_prototype/_tier3/_proximal_other_labels_count.py
@@ -0,0 +1,41 @@
+from .._tier0 import plugin_function, Image, create_none
+import numpy as np
+
+@plugin_function(categories=['label measurement', 'combine', 'map', 'label comparison'], output_creator=create_none)
+def proximal_other_labels_count(label_image:Image, other_label_image:Image, count_vector:Image = None, maximum_distance=25):
+    """
+    Count number of labels within a given radius in an other label image and returns the result as vector.
+
+    Parameters
+    ----------
+    label_image: Image
+    other_label_image: Image
+    count_vector: Image, optional
+        vector (num_labels + 1 long) where the values will be written in. The first column (index 0, value 0)
+        corresponds to background.
+    maximum_distance: Number, optional
+        maximum distance in pixels
+
+    Returns
+    -------
+    count_vector
+
+    """
+    from .._tier9 import centroids_of_labels
+    from .._tier1 import generate_distance_matrix, set_row, set_column, smaller_or_equal_constant, sum_y_projection
+    centroids_a = centroids_of_labels(label_image)
+    centroids_b = centroids_of_labels(other_label_image)
+
+    distance_matrix = generate_distance_matrix(centroids_a, centroids_b)
+
+    # ignore distance to background
+    set_row(distance_matrix, 0, np.finfo(np.float32).max)
+    set_column(distance_matrix, 0, np.finfo(np.float32).max)
+
+    # threshold matrix
+    binary_matrix = smaller_or_equal_constant(distance_matrix, constant=maximum_distance)
+
+    # count objects in each column of that matrix
+    count_vector = sum_y_projection(binary_matrix, count_vector)
+
+    return count_vector
