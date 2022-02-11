35304a3839658437c2f63b0847ac959921fb77fb
Robert Haase <haesleinhuepf@users.noreply.github.com>
2022-02-11 15:04:48 +0100

    fancy indexing + many tests against numpy

--- pyclesperanto_prototype/_tier0/_array_operators.py
+++ pyclesperanto_prototype/_tier0/_array_operators.py
@@ -237,12 +237,14 @@ class ArrayOperators():
         return super().__setitem__(index, value)
 
     def __getitem__(self, index):
+        print("__getitem__")
         result = None
         if isinstance(index, list):
             index = tuple(index)
         if isinstance(index, (tuple, np.ndarray)) and index[0] is not None and isinstance(index[0], (tuple, list, np.ndarray)):
             if len(index) == len(self.shape):
                 if len(index[0]) > 0:
+                    print("wtf")
                     # switch xy in 2D / xz in 3D, because clesperanto expects an X-Y-Z array;
                     # see also https://github.com/clEsperanto/pyclesperanto_prototype/issues/49
                     index = list(index)
@@ -257,9 +259,79 @@ class ArrayOperators():
                     return []
 
         if result is None:
+            if isinstance(index, tuple):
+                print(index)
+
+                if len(self.shape) > 2:  # 3D image
+                    if len(index) > 2:
+                        x_range = index[2]
+                    else:
+                        x_range = slice(None, None, None)
+
+                    if len(index) > 1:
+                        y_range = index[1]
+                    else:
+                        y_range = slice(None, None, None)
+
+                    if len(index) > 0:
+                        z_range = index[0]
+                    else:
+                        z_range = slice(None, None, None)
+                else:
+                    if len(index) > 1:
+                        x_range = index[1]
+                    else:
+                        x_range = slice(None, None, None)
+
+                    if len(index) > 0:
+                        y_range = index[0]
+                    else:
+                        y_range = slice(None, None, None)
+
+                    z_range = slice(None, None, None)
+
+                eliminate_x = False
+                eliminate_y = False
+                eliminate_z = False
+                if isinstance(x_range, int):
+                    x_range = slice(x_range, x_range + 1, 1)
+                    eliminate_x = True
+                if isinstance(y_range, int):
+                    y_range = slice(y_range, y_range + 1, 1)
+                    eliminate_y = True
+                if isinstance(z_range, int):
+                    z_range = slice(z_range, z_range + 1, 1)
+                    eliminate_z = True
+
+                print("Cropping ranges", x_range, y_range, z_range)
+
+                from .._tier1 import range
+                result = range(self, start_x=x_range.start, stop_x=x_range.stop, step_x=x_range.step,
+                               start_y=y_range.start, stop_y=y_range.stop, step_y=y_range.step,
+                               start_z=z_range.start, stop_z=z_range.stop, step_z=z_range.step)
+
+                from .._tier0 import create
+                from .._tier1 import copy_slice, copy_vertical_slice, copy_horizontal_slice
+                if eliminate_x:
+                    output = create(result.shape[:2])
+                    result = copy_vertical_slice(result, output)
+                if eliminate_y:
+                    output = create((result.shape[0],result.shape[2]))
+                    result = copy_horizontal_slice(result, output)
+                if eliminate_z:
+                    print("eliminating z")
+                    output = create(result.shape[1:])
+                    result = copy_slice(result, output)
+
+        if result is None:
+
             if hasattr(super(), "__getitem__"):
+                print(type(index))
+                print(index)
+                print("to pyopencl")
                 result = super().__getitem__(index)
             else:
+                print("to numpy")
                 result = self.get().__getitem__(index)
 
         if result.size == 1 and isinstance(result, (ArrayOperators)):
---
+++ tests/test_range.py
@@ -0,0 +1,357 @@
+def test_range1():
+    import numpy as np
+    import pyclesperanto_prototype as cle
+
+    image = cle.push(np.asarray([
+        [0, 1, 2],
+        [2, 3, 4],
+        [5, 6, 7]
+    ]))
+
+    reference = cle.push(np.asarray([
+        [3, 4],
+        [6, 7]
+    ]))
+
+    crop = image[1:, 1:]
+
+    print(reference)
+    print(crop)
+
+    assert np.array_equal(crop, reference)
+
+def test_range2():
+    import numpy as np
+    import pyclesperanto_prototype as cle
+
+    image = cle.push(np.asarray([
+        [0, 1, 2],
+        [2, 3, 4],
+        [5, 6, 7]
+    ]))
+
+    reference = cle.push(np.asarray([
+        [0, 1],
+        [2, 3]
+    ]))
+
+    crop = image[:2, :2]
+
+    print(reference)
+    print(crop)
+
+    assert np.array_equal(crop, reference)
+
+def test_range3():
+    import numpy as np
+    import pyclesperanto_prototype as cle
+
+    image = cle.push(np.asarray([[
+        [0, 1, 2],
+        [2, 3, 4],
+        [5, 6, 7]
+    ]]))
+
+    reference = cle.push(np.asarray([[
+        [3, 4],
+        [6, 7]
+    ]]))
+
+    crop = image[:,1:, 1:]
+
+    print(reference)
+    print(crop)
+
+    assert np.array_equal(crop, reference)
+
+def test_range4():
+    import numpy as np
+    import pyclesperanto_prototype as cle
+
+    image = cle.push(np.asarray([[
+        [0, 1, 2],
+        [2, 3, 4],
+        [5, 6, 7]
+    ]]))
+
+    reference = cle.push(np.asarray([[
+        [0, 1],
+        [2, 3]
+    ]]))
+
+    crop = image[:,:2, :2]
+
+    print(reference)
+    print(crop)
+
+    assert np.array_equal(crop, reference)
+
+def test_range5():
+    import numpy as np
+    import pyclesperanto_prototype as cle
+
+    image = cle.push(np.random.random((10,20,30)))
+
+    crop = image[:5]
+
+    assert crop.shape[0] == 5
+    assert crop.shape[1] == 20
+    assert crop.shape[2] == 30
+
+def test_range6():
+    import numpy as np
+    import pyclesperanto_prototype as cle
+
+    image = cle.push(np.random.random((10,20,30)))
+
+    crop = image[5]
+
+    assert crop.shape[0] == 20
+    assert crop.shape[1] == 30
+    assert len(crop.shape) == 2
+
+def test_range7():
+    import numpy as np
+    import pyclesperanto_prototype as cle
+
+    image = cle.push(np.random.random((10,20,30)))
+
+    crop = image[5, :]
+
+    assert crop.shape[0] == 20
+    assert crop.shape[1] == 30
+    assert len(crop.shape) == 2
+
+def test_range8():
+    import numpy as np
+    import pyclesperanto_prototype as cle
+
+    image = cle.push(np.random.random((10,20,30)))
+
+    crop = image[:,:, 5]
+
+    assert crop.shape[0] == 10
+    assert crop.shape[1] == 20
+    assert len(crop.shape) == 2
+
+def test_range9():
+    import numpy as np
+    import pyclesperanto_prototype as cle
+
+    image = cle.push(np.random.random((10,20,30)))
+
+    crop = image[:, 5]
+
+    assert crop.shape[0] == 10
+    assert crop.shape[1] == 30
+    assert len(crop.shape) == 2
+
+def test_range_against_numpy_1():
+    import numpy as np
+    import pyclesperanto_prototype as cle
+
+    input = np.random.random((2,3,4))
+    input_gpu = cle.push(input)
+
+    reference = input[1,:,:]
+    result = input_gpu[1,:,:]
+
+    print(reference)
+    print(result)
+
+    assert np.allclose(reference, result, 0.0001)
+
+def test_range_against_numpy_2():
+    import numpy as np
+    import pyclesperanto_prototype as cle
+
+    input = np.random.random((2,3,4))
+    input_gpu = cle.push(input)
+
+    reference = input[:,1,:]
+    result = input_gpu[:,1,:]
+
+    print(reference)
+    print(result)
+
+    assert np.allclose(reference, result, 0.0001)
+
+def test_range_against_numpy_3():
+    import numpy as np
+    import pyclesperanto_prototype as cle
+
+    input = np.random.random((2,3,4))
+    input_gpu = cle.push(input)
+
+    reference = input[:,:,1]
+    result = input_gpu[:,:,1]
+
+    print(reference)
+    print(result)
+
+    assert np.allclose(reference, result, 0.0001)
+
+def test_range_against_numpy_4():
+    import numpy as np
+    import pyclesperanto_prototype as cle
+
+    input = np.random.random((2,3,4))
+    input_gpu = cle.push(input)
+
+    reference = input[1,:]
+    result = input_gpu[1,:]
+
+    print(reference)
+    print(result)
+
+    assert np.allclose(reference, result, 0.0001)
+
+def test_range_against_numpy_5():
+    import numpy as np
+    import pyclesperanto_prototype as cle
+
+    input = np.random.random((2,3,4))
+    input_gpu = cle.push(input)
+
+    reference = input[1]
+    result = input_gpu[1]
+
+    print(reference)
+    print(result)
+
+    assert np.allclose(reference, result, 0.0001)
+
+def test_range_against_numpy_6():
+    import numpy as np
+    import pyclesperanto_prototype as cle
+
+    input = np.random.random((10,20,30))
+    input_gpu = cle.push(input)
+
+    reference = input[2:5,:,:]
+    result = input_gpu[2:5,:,:]
+
+    print(reference)
+    print(result)
+
+    assert np.allclose(reference, result, 0.0001)
+
+def test_range_against_numpy_7():
+    import numpy as np
+    import pyclesperanto_prototype as cle
+
+    input = np.random.random((10,20,30))
+    input_gpu = cle.push(input)
+
+    reference = input[:,10:15,:]
+    result = input_gpu[:,10:15,:]
+
+    print(reference)
+    print(result)
+
+    assert np.allclose(reference, result, 0.0001)
+
+def test_range_against_numpy_8():
+    import numpy as np
+    import pyclesperanto_prototype as cle
+
+    input = np.random.random((10,20,30))
+    input_gpu = cle.push(input)
+
+    reference = input[:,:,10:15]
+    result = input_gpu[:,:,10:15]
+
+    print(reference)
+    print(result)
+
+    assert np.allclose(reference, result, 0.0001)
+
+def test_range_against_numpy_9():
+    import numpy as np
+    import pyclesperanto_prototype as cle
+
+    input = np.random.random((10,20,30))
+    input_gpu = cle.push(input)
+
+    reference = input[:5,:,:]
+    result = input_gpu[:5,:,:]
+
+    print(reference)
+    print(result)
+
+    assert np.allclose(reference, result, 0.0001)
+
+def test_range_against_numpy_10():
+    import numpy as np
+    import pyclesperanto_prototype as cle
+
+    input = np.random.random((10,20,30))
+    input_gpu = cle.push(input)
+
+    reference = input[:,:5,:]
+    result = input_gpu[:,:5,:]
+
+    print(reference)
+    print(result)
+
+    assert np.allclose(reference, result, 0.0001)
+
+def test_range_against_numpy_11():
+    import numpy as np
+    import pyclesperanto_prototype as cle
+
+    input = np.random.random((10,20,30))
+    input_gpu = cle.push(input)
+
+    reference = input[:,:,:5]
+    result = input_gpu[:,:,:5]
+
+    print(reference)
+    print(result)
+
+    assert np.allclose(reference, result, 0.0001)
+
+def test_range_against_numpy_12():
+    import numpy as np
+    import pyclesperanto_prototype as cle
+
+    input = np.random.random((10,20,30))
+    input_gpu = cle.push(input)
+
+    reference = input[5:,:,:]
+    result = input_gpu[5:,:,:]
+
+    print(reference)
+    print(result)
+
+    assert np.allclose(reference, result, 0.0001)
+
+def test_range_against_numpy_14():
+    import numpy as np
+    import pyclesperanto_prototype as cle
+
+    input = np.random.random((10,20,30))
+    input_gpu = cle.push(input)
+
+    reference = input[:,5:,:]
+    result = input_gpu[:,5:,:]
+
+    print(reference)
+    print(result)
+
+    assert np.allclose(reference, result, 0.0001)
+
+def test_range_against_numpy_15():
+    import numpy as np
+    import pyclesperanto_prototype as cle
+
+    input = np.random.random((10,20,30))
+    input_gpu = cle.push(input)
+
+    reference = input[:,:,5:]
+    result = input_gpu[:,:,5:]
+
+    print(reference)
+    print(result)
+
+    assert np.allclose(reference, result, 0.0001)
