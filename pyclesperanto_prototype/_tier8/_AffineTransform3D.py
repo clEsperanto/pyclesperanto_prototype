deskew_x fix and define shear factor calculation

pyclesperanto_prototype/_tier8/_AffineTransform3D.py
@@ -231,7 +231,7 @@ class AffineTransform3D:
         ]))
         return self
 
-    def shear_in_y_plane(self,angle_x_in_degrees: float = 0, angle_z_in_degrees: float = 0 ):
+    def shear_in_y_plane(self,angle_x_in_degrees: float = 90, angle_z_in_degrees: float = 90 ,voxel_size_x:float = 1,voxel_size_y:float = 1,voxel_size_z:float = 1):
         """Shear image in Y-plane (a.k.a. XZ-plane) along X and/or Z direction.
 
         Angles need to be specified in degrees. To convert radians to degrees use this formula:
@@ -248,11 +248,11 @@ class AffineTransform3D:
         -------
         self
         """
-        assert(angle_x_in_degrees >-90 and angle_x_in_degrees<90), "shear angle must be between 90 and -90 degrees"
-        assert(angle_z_in_degrees >-90 and angle_z_in_degrees<90), "shear angle must be between 90 and -90 degrees"
+        assert(angle_x_in_degrees >=-90 and angle_x_in_degrees<=90), "shear angle must be between 90 and -90 degrees"
+        assert(angle_z_in_degrees >=-90 and angle_z_in_degrees<=90), "shear angle must be between 90 and -90 degrees"
 
-        shear_factor_xy = shear_angle_to_shear_factor(angle_x_in_degrees)
-        shear_factor_zy = shear_angle_to_shear_factor(angle_z_in_degrees)
+        shear_factor_xy = shear_angle_to_shear_factor(angle_x_in_degrees,voxel_size_x, voxel_size_z)
+        shear_factor_zy = shear_angle_to_shear_factor(angle_z_in_degrees,voxel_size_z, voxel_size_x)
 
         # shearing
         self._pre_concatenate(np.asarray([
@@ -263,7 +263,7 @@ class AffineTransform3D:
         ]))
         return self
 
-    def shear_in_x_plane(self,angle_y_in_degrees: float = 0, angle_z_in_degrees: float = 0,voxel_size_y:float = 1,voxel_size_z:float = 1):
+    def shear_in_x_plane(self,angle_y_in_degrees: float = 90, angle_z_in_degrees: float = 90,voxel_size_x:float = 1,voxel_size_y:float = 1,voxel_size_z:float = 1):
         """Shear image in X-plane (a.k.a. YZ-plane) along Y and/or Z direction.
 
         Tip: This can be used for single objective lightsheet image reconstruction / deskewing.
@@ -283,11 +283,11 @@ class AffineTransform3D:
         -------
         self
         """
-        assert(angle_y_in_degrees >-90 and angle_y_in_degrees<90), "shear angle must be between 90 and -90 degrees"
-        assert(angle_z_in_degrees >-90 and angle_z_in_degrees<90), "shear angle must be between 90 and -90 degrees"
+        assert(angle_y_in_degrees >=-90 and angle_y_in_degrees<=90), "shear angle must be between 90 and -90 degrees"
+        assert(angle_z_in_degrees >=-90 and angle_z_in_degrees<=90), "shear angle must be between 90 and -90 degrees"
         
         shear_factor_yx = shear_angle_to_shear_factor(angle_y_in_degrees,voxel_size_y,voxel_size_z)
-        shear_factor_zx = shear_angle_to_shear_factor(angle_z_in_degrees,voxel_size_y,voxel_size_z)
+        shear_factor_zx = shear_angle_to_shear_factor(angle_z_in_degrees,voxel_size_z, voxel_size_y)
 
         # shearing
         self._pre_concatenate(np.asarray([
@@ -301,15 +301,8 @@ class AffineTransform3D:
     def _deskew_y(self, angle_in_degrees:float, voxel_size_x: float = 1,
                   voxel_size_y: float = 1, voxel_size_z: float = 1, scale_factor: float = 1):
 
-        self.shear_in_x_plane(angle_y_in_degrees=90 - angle_in_degrees)
-
-        # defining shear factor wrt voxel size
-        # This could potentially go into _utilities ?
-        shear_factor = math.sin((90 - angle_in_degrees) * math.pi / 180.0) * (voxel_size_z / voxel_size_y)
-        self._matrix[1, 2] = shear_factor
-
-        # rotate the stack to get proper Z-planes; rotate 90 - angle around X-axis
-        # self.rotate(angle_in_degrees = 90 -angle_in_degrees, axis=0)
+        self.shear_in_x_plane(angle_y_in_degrees= angle_in_degrees,
+                              voxel_size_y = voxel_size_y,voxel_size_z = voxel_size_z)
 
         # make voxels isotropic, calculate the new scaling factor for Z after shearing
         # https://github.com/tlambert03/napari-ndtiffs/blob/092acbd92bfdbf3ecb1eb9c7fc146411ad9e6aae/napari_ndtiffs/affine.py#L57
@@ -325,20 +318,17 @@ class AffineTransform3D:
     def _deskew_x(self, angle_in_degrees:float, voxel_size_x: float = 1,
                   voxel_size_y: float = 1, voxel_size_z: float = 1, scale_factor: float = 1):
         
-        self.shear_in_y_plane(angle_x_in_degrees = 90 - angle_in_degrees)
-
-        # rotate the stack to get proper Z-planes; rotate 90 - angle around Y-axis
-        self.rotate(angle_in_degrees=-(90 - angle_in_degrees), axis=1)
-
-        # make voxels isotropic, calculate the new scaling factor for Z after shearing
-        # https://github.com/tlambert03/napari-ndtiffs/blob/092acbd92bfdbf3ecb1eb9c7fc146411ad9e6aae/napari_ndtiffs/affine.py#L57
+        self.shear_in_y_plane(angle_x_in_degrees = angle_in_degrees,
+                              voxel_size_x = voxel_size_x, voxel_size_z = voxel_size_z)
+        #make voxels isotropic, calculate the new scaling factor for Z after shearing
+        #https://github.com/tlambert03/napari-ndtiffs/blob/092acbd92bfdbf3ecb1eb9c7fc146411ad9e6aae/napari_ndtiffs/affine.py#L57
         new_dz = math.sin(angle_in_degrees * math.pi / 180.0) * voxel_size_z
         scale_factor_z = (new_dz / voxel_size_x) * scale_factor
+
         self.scale(scale_x=scale_factor, scale_y=scale_factor, scale_z=scale_factor_z)
 
         # correct orientation so that the new Z-plane goes proximal-distal from the objective.
-        self.rotate(angle_in_degrees=-90, axis=1)
-        self.rotate(angle_in_degrees=90, axis=2)
+        self.rotate(angle_in_degrees =  angle_in_degrees, axis=1)      
 
         return self
 

pyclesperanto_prototype/_tier8/_utilities.py
@@ -85,31 +85,34 @@ def transform_from_string(text: str, image: np.ndarray):
     return at
 
 
-def shear_angle_to_shear_factor(angle_in_degrees,voxel_size_1:float=1,voxel_size_2:float=1):
+def shear_angle_to_shear_factor(angle_in_degrees,lateral_voxel_size:float=1,axial_voxel_size:float=1):
     """
     Converts a shearing angle into a shearing factor
 
     Parameters
     ----------
     angle_in_degrees: float
-
+    lateral_voxel_size (float, optional):voxel size in lateral axis in reference to plane being processed. Defaults to 1.
+    axial_voxel_size (float, optional): voxel size in axial axis in reference to plane being processed. Defaults to 1.
     Returns
     -------
     float
     """
-    return math.sin((90 - angle_in_degrees) * math.pi / 180.0) * (voxel_size_2 / voxel_size_1)
+    return math.sin((90 - angle_in_degrees) * math.pi / 180.0) * (axial_voxel_size / lateral_voxel_size)
 
-#TODO: MODIFY
-def shear_factor_to_shear_angle(shear_factor):
+
+def shear_factor_to_shear_angle(shear_factor:float,lateral_voxel_size:float=1,axial_voxel_size:float=1):
     """
     Converts a shearing angle into a shearing factor
 
-    Parameters
-    ----------
-    shear_factor: float
+    Args:
+        shear_factor (float)
+        lateral_voxel_size (float, optional):voxel size in lateral axis in reference to plane being processed. Defaults to 1.
+        axial_voxel_size (float, optional): voxel size in axial axis in reference to plane being processed. Defaults to 1.
 
-    Returns
-    -------
-    float
+    Returns:
+        shear_angle(float)
     """
-    return  - math.atan(1.0 / shear_factor) * 180 / math.pi + 90
+
+    
+    return  round((90 - math.asin(shear_factor*(lateral_voxel_size/axial_voxel_size))*180/math.pi),2)