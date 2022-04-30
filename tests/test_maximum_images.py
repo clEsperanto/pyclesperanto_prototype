122ef0ea14a051ff8817c93d9ef5803acc03762a
Robert Haase <haesleinhuepf@users.noreply.github.com>
2022-04-30 15:15:32 +0200

    bugfix: types issue in maximum_images

--- tests/test_maximum_images.py
+++ tests/test_maximum_images.py
@@ -34,3 +34,30 @@ def test_maximum_images():
     print(a)
 
     assert (np.array_equal(a, b))
+
+def test_maximum_images_with_types():
+    import numpy as np
+    test1 = cle.push(np.asarray([
+        [0, 3, 3, 4, 0],
+    ]))
+    test2 = np.asarray([
+        [0, 2, 1, 3, 0],
+    ])
+
+    reference = cle.push(np.asarray([
+        [0, 2, 1, 3, 0],
+    ]))
+
+    result1 = cle.maximum_images(test1.astype(int), test2.astype(int))
+    result2 = cle.maximum_images(test1.astype(float), test2.astype(float))
+    result3 = cle.maximum_images(test1.astype(int), test2.astype(float))
+    result4 = cle.maximum_images(test1.astype(np.uint16), test2.astype(np.float32))
+    result5 = cle.maximum_images(test1.astype(np.uint32), test2.astype(np.float32))
+
+    assert np.array_equal(result1, reference)
+    assert np.array_equal(result2, reference)
+    assert np.array_equal(result3, reference)
+    assert np.array_equal(result4, reference)
+    assert np.array_equal(result5, reference)
+
+
