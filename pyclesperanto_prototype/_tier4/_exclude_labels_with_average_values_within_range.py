1a7a5b8d6b0758f3c176429c2b6b9b10de6502fb
Robert Haase <haesleinhuepf@users.noreply.github.com>
2022-05-01 09:56:32 +0200

    added exclude_labels_with_average_values_within_range

--- pyclesperanto_prototype/_tier4/_exclude_labels_with_average_values_within_range.py
+++ pyclesperanto_prototype/_tier4/_exclude_labels_with_average_values_within_range.py
@@ -5,7 +5,7 @@ from .._tier0 import Image
 from .._tier0 import create_labels_like
 
 @plugin_function(output_creator=create_labels_like, categories=['label processing', 'combine', 'in assistant'])
-def exclude_labels_with_average_values_out_of_range(values_image : Image, label_map_input: Image, label_map_destination: Image = None, minimum_value_range : float = 0, maximum_value_range : float = 100) -> Image:
+def exclude_labels_with_average_values_within_range(values_image : Image, label_map_input: Image, label_map_destination: Image = None, minimum_value_range : float = 0, maximum_value_range : float = 100) -> Image:
     """This operation removes labels from a labelmap and renumbers the
     remaining labels.
 
@@ -25,6 +25,6 @@ def exclude_labels_with_average_values_out_of_range(values_image : Image, label_
     from .._tier9 import statistics_of_background_and_labelled_pixels
     statistics = statistics_of_background_and_labelled_pixels(values_image, label_map_input)
     values_vector = np.asarray([statistics["mean_intensity"]])
-    from .._tier3 import exclude_labels_with_values_out_of_range
+    from .._tier3 import exclude_labels_with_values_within_range
 
-    return exclude_labels_with_values_out_of_range(values_vector, label_map_input, label_map_destination, minimum_value_range, maximum_value_range)
+    return exclude_labels_with_values_within_range(values_vector, label_map_input, label_map_destination, minimum_value_range, maximum_value_range)
