c9b1d923583b87e93f95d5ab07ab18a0f1c00124
Robert Haase <haesleinhuepf@users.noreply.github.com>
2022-08-06 21:13:17 +0200

    added array_equiv alias

--- pyclesperanto_prototype/_tier5/__init__.py
+++ pyclesperanto_prototype/_tier5/__init__.py
@@ -1,4 +1,5 @@
 from ._array_equal import array_equal
+from ._array_equal import array_equal as array_equiv
 from ._closing_labels import closing_labels
 from ._connected_components_labeling_diamond import connected_components_labeling_diamond
 from ._masked_voronoi_labeling import masked_voronoi_labeling
