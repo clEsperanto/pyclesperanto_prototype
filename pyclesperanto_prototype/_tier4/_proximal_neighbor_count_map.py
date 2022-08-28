55c37572e8f428b16e7c9f53dcddf83210adefeb
Robert Haase <haesleinhuepf@users.noreply.github.com>
2022-08-28 12:06:33 +0200

    added short cut to proximal_neighbor_count as vector

--- pyclesperanto_prototype/_tier4/_proximal_neighbor_count_map.py
+++ pyclesperanto_prototype/_tier4/_proximal_neighbor_count_map.py
@@ -9,9 +9,7 @@ import numpy as np
 def proximal_neighbor_count_map(source : Image, destination : Image = None, min_distance : float = 0, max_distance : float = np.finfo(np.float32).max) -> Image:
     """Takes a label map, determines which labels are within a give distance range
     and replaces every label with the number of neighboring labels.
-    
-     
-    
+
     Parameters
     ----------
     source : Image
@@ -29,17 +27,8 @@ def proximal_neighbor_count_map(source : Image, destination : Image = None, min_
     ----------
     .. [1] https://clij.github.io/clij2-docs/reference_proximalNeighborCountMap
     """
-    from .._tier1 import set_column
-    from .._tier9 import centroids_of_labels
-    from .._tier1 import generate_distance_matrix
+    from ._proximal_neighbor_count import proximal_neighbor_count
+    number_of_touching_neighbors_vector = proximal_neighbor_count(source, max_distance=max_distance, min_distance=min_distance)
 
-    pointlist = centroids_of_labels(source)
-    distance_matrix = generate_distance_matrix(pointlist, pointlist)
-
-    touch_matrix = generate_proximal_neighbors_matrix(distance_matrix, min_distance=min_distance, max_distance=max_distance)
-    number_of_touching_neighbors_vector = count_touching_neighbors(touch_matrix)
-
-    # ignore how many objects touch the background
-    set_column(number_of_touching_neighbors_vector, 0, 0)
     replace_intensities(source, number_of_touching_neighbors_vector, destination)
     return destination
