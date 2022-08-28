7eb0ef1ed06f4ebd2fb657c30bf2aaef37dd0c27
Robert Haase <haesleinhuepf@users.noreply.github.com>
2022-08-28 10:22:37 +0200

    bugfix: the touch portion mesh need to consider both directions and average them

--- pyclesperanto_prototype/_tier4/_generate_touch_portion_matrix.py
+++ pyclesperanto_prototype/_tier4/_generate_touch_portion_matrix.py
@@ -10,6 +10,10 @@ def generate_touch_portion_matrix(label_map:Image, touch_portion_matrix_destinat
     pixels on the object's border, excluding the image border.
     Put these numbers in a symmetric touch count matrix.
 
+    Note: This matrix is not necessarily symmetric because touch portion depends on the amount
+    of label border pixels (divident). Thus, for each edge between labels A and B, two number
+    exist: one ratio to label A and one ratio to label B.
+
     Parameters
     ----------
     label_map: Image
