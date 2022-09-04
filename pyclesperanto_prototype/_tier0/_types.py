72401f93773bee18767aeecf5b11080780177edb
PRADEEP\Pradeep <pradeep.rajasekhar@gmail.com>
2022-09-04 15:26:36 +1000

    add support for resource backed dask array

--- pyclesperanto_prototype/_tier0/_types.py
+++ pyclesperanto_prototype/_tier0/_types.py
@@ -6,4 +6,4 @@ from typing import Union
 Image = Union[np.ndarray, OCLArray, cl.Image, _OCLImage]
 
 def is_image(object):
-    return isinstance(object, np.ndarray) or isinstance(object, tuple)  or isinstance(object, list) or isinstance(object, OCLArray) or str(type(object)) in ["<class 'cupy._core.core.ndarray'>", "<class 'dask.array.core.Array'>", "<class 'xarray.core.dataarray.DataArray'>"]
+    return isinstance(object, np.ndarray) or isinstance(object, tuple)  or isinstance(object, list) or isinstance(object, OCLArray) or str(type(object)) in ["<class 'cupy._core.core.ndarray'>", "<class 'dask.array.core.Array'>", "<class 'xarray.core.dataarray.DataArray'>","<class 'resource_backed_dask_array.ResourceBackedDaskArray'>"]
