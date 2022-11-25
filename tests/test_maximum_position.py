7cf9da47487d17ff5fd899fbd0708fc7ef07627d
Graham Ross <graham.ross@crick.ac.uk>
2022-11-25 16:17:50 +0000

    Added remaining tests

--- tests/test_maximum_position.py
+++ tests/test_maximum_position.py
@@ -2,18 +2,20 @@ import pyclesperanto_prototype as cle
 import numpy as np
 
 def test_maximum_position():
-    np_input = np.asarray([
-        [
-            [1, 1, 1, 1,]
-            [1, 1, 2, 1,]
-            [1, 1, 1, 1,]
-        ],
-            [[1, 1, 1, 1,]
-            [1, 1, 1, 1,]
-            [2, 1, 1, 1,]]
+    np_input= np.asarray([
+            [
+                [1, 2, 3, 10],
+                [4, 16, 6, 11],
+                [7, 8, 9, 12]
+            ],
+            [
+                [1, 2, 3, 13],
+                [4, 5, 6, 16],
+                [7, 8, 9, 15]
+            ]
         ])
 
-    reference = (0, 1, 2)
+    reference = (0, 1, 1)
     gpu_input = cle.push(np_input)
     result = cle.maximum_position(gpu_input)
 
---
+++ tests/test_x_position_of_maximum_x_projection.py
@@ -0,0 +1,61 @@
+import pyclesperanto_prototype as cle
+import numpy as np
+
+test1 = cle.push(np.asarray([
+    [
+        [1, 0, 0, 0, 9],
+        [0, 2, 0, 8, 0],
+        [3, 0, 1, 0, 10],
+        [0, 4, 0, 7, 0],
+    ], [
+        [0, 2, 0, 8, 0],
+        [1, 0, 0, 0, 9],
+        [3, 0, 1, 0, 10],
+        [0, 4, 0, 7, 0],
+    ], [
+        [0, 2, 0, 8, 0],
+        [3, 0, 1, 0, 10],
+        [0, 4, 0, 7, 0],
+        [1, 0, 0, 0, 9],
+    ]
+]))
+
+reference = cle.push(np.asarray([
+    [4, 3, 3,]
+    [3, 4, 4,]
+    [4, 4, 3,]
+    [3, 3, 4,]
+]))
+
+def test_x_position_of_maximum_x_projection():
+
+    result = cle.create(reference)
+    cle.x_position_of_maximum_x_projection(test1, result)
+
+    a = cle.pull(result)
+    b = cle.pull(reference)
+
+    print(a)
+
+    assert (np.array_equal(a, b))
+
+
+def test_x_position_of_maximum_x_projection_creator():
+    result = cle.x_position_of_maximum_x_projection(test1)
+
+    a = cle.pull(result)
+    b = cle.pull(reference)
+
+    print(a)
+
+    assert (np.array_equal(a, b))
+
+def test_x_position_of_maximum_x_projection_creator_passing_none():
+    result = cle.x_position_of_maximum_x_projection(test1, None)
+
+    a = cle.pull(result)
+    b = cle.pull(reference)
+
+    print(a)
+
+    assert (np.array_equal(a, b))
