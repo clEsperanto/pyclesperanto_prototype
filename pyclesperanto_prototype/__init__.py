0f037b6f24faf7c43f5837497dcc2ad3569a3add
Robert Haase <haesleinhuepf@users.noreply.github.com>
2022-12-01 21:02:43 +0100

    add types to namespace

--- pyclesperanto_prototype/__init__.py
+++ pyclesperanto_prototype/__init__.py
@@ -12,3 +12,16 @@ from ._tier11 import *
 
 __version__ = "0.19.4"
 __common_alias__ = "cle"
+
+# array API interoperability
+import numpy as np
+# bool = np.bool
+uint8 = np.uint8
+uint16 = np.uint16
+uint32 = np.uint32
+uint64 = np.uint64
+int8 = np.int8
+int16 = np.int16
+int32 = np.int32
+int64 = np.int64
+float32 = np.float32
