0ccbeb35e6cf4eda5c7b5ef574eed79b4fd3f465
Robert Haase <haesleinhuepf@users.noreply.github.com>
2022-12-02 08:15:40 +0100

    added one more test

--- tests/test_maximum_position.py
+++ tests/test_maximum_position.py
@@ -22,6 +22,18 @@ def test_maximum_position_3d():
 
     assert (result == reference)
 
+def test_maximum_position_3d_2():
+    np_input = np.zeros((10,11,12))
+
+    np_input[1, 2, 3] = 1
+    np_input[6, 7, 8] = 1
+
+    reference = ndimage.maximum_position(np_input)
+    gpu_input = cle.push(np_input)
+    result = cle.maximum_position(gpu_input)
+
+    assert (result == reference)
+
 def test_maximum_position_2d():
     np_input= np.asarray([
                 [1, 2, 3, 10],
