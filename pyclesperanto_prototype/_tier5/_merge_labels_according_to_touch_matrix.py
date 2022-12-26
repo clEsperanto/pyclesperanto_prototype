306588fb520a9b33bab3c143cdf54b4d2fcf8af2
Robert Haase <haesleinhuepf@users.noreply.github.com>
2022-12-26 15:23:38 +0100

    bugfix: merge didn't work in conplex scenarios, cleaned code

--- pyclesperanto_prototype/_tier5/_merge_labels_according_to_touch_matrix.py
+++ pyclesperanto_prototype/_tier5/_merge_labels_according_to_touch_matrix.py
@@ -14,15 +14,13 @@ def merge_labels_according_to_touch_matrix(labels:Image, touch_matrix:Image, lab
     -------
     labels_destination
     """
-
-
     from .._tier0 import create
     from .._tier1 import multiply_images, set_ramp_y, maximum_y_projection, set_ramp_x, maximum_images, \
-        replace_intensities
+       replace_intensities, transpose_xy, copy
     from .._tier3 import relabel_sequential
     from .._tier2 import symmetric_maximum_matrix
+    from ._array_equal import array_equal
 
-    # print("touching", touch_matrix[24, 26])
     touch_matrix = symmetric_maximum_matrix(touch_matrix)
 
     # vector with 0, 1, 2, 3, ...
@@ -31,29 +29,29 @@ def merge_labels_according_to_touch_matrix(labels:Image, touch_matrix:Image, lab
 
     # multiply vector with touch matrix to get a touch-matrix where
     # labels that should be merged have the value of the "other" label
-    touch_matrix = multiply_images(touch_matrix, label_id_vector)
+    compare_to = transpose_xy(label_id_vector)
 
-    # the new list of label-IDs for each label is the maximum-y-projection of the matrix
-    # if labels 2 and 3 should be merged, both have then label-ID 3
-    label_id_vector = create([1, touch_matrix.shape[0]])
-    maximum_y_projection(touch_matrix, label_id_vector)
+    while True:
+        label_id_matrix = multiply_images(touch_matrix, label_id_vector)
 
-    # replace 0s in this vector with a large number
-    # num_labels = labels.max()
-    # label_id_vector = cle.replace_intensity(label_id_vector, value_to_replace=0, value_replacement=num_labels)
+        # the new list of label-IDs for each label is the maximum-y-projection of the matrix
+        # if labels 2 and 3 should be merged, both have then label-ID 3
+        label_id_vector = create([1, label_id_matrix.shape[0]])
+        maximum_y_projection(label_id_matrix, label_id_vector)
 
-    # determine minimum of original label versus new label (or large number, in case they should not be merged)
-    original_labels = create(label_id_vector.shape)
-    set_ramp_x(original_labels)
-    # renumber the labels
-    new_labels = maximum_images(label_id_vector, original_labels)
+        # determine minimum of original label versus new label (or large number, in case they should not be merged)
+        original_labels = create(label_id_vector.shape)
+        set_ramp_x(original_labels)
+        # renumber the labels
+        new_labels = maximum_images(label_id_vector, original_labels)
+
+        # conversion criterion; if the new labels don't change anymore, we are done
+        if array_equal(compare_to, new_labels):
+            break
+        copy(new_labels, compare_to)
+        label_id_vector = transpose_xy(new_labels)
 
     new_labels = relabel_sequential(new_labels)
-
-    # print("li", label_id_vector)
-    # print("ol", original_labels)
-    # print("nl", new_labels)
-
 
     # write the new labels into the label image
     return replace_intensities(labels, new_labels, labels_destination)
