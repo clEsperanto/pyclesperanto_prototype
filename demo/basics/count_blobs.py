3822ff90059a402d08d42906ea3c723e14866efe
Robert Haase <haesleinhuepf@users.noreply.github.com>
2023-02-27 17:31:10 +0100

    updated example notebooks

--- demo/basics/count_blobs.py
+++ demo/basics/count_blobs.py
@@ -16,10 +16,8 @@ blurred = cle.gaussian_blur(inverted, sigma_x=1, sigma_y=1)
 binary = cle.threshold_otsu(blurred)
 labeled = cle.connected_components_labeling_box(binary)
 
-# The maxmium intensity in a label image corresponds to the number of objects
-num_labels = cle.maximum_of_all_pixels(labeled)
-
-# print out result
+# The maximium intensity in a label image corresponds to the number of objects
+num_labels = labeled.max()
 print("Num objects in the image: " + str(num_labels))
 
 # for debugging: print out image
