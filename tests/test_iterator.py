8b184757c7607e3a34a7e76a21de838ec48d8a67
Robert Haase <haesleinhuepf@users.noreply.github.com>
2023-03-24 18:42:47 +0100

    add test for enumerate and zip

--- tests/test_iterator.py
+++ tests/test_iterator.py
@@ -9,3 +9,29 @@ def test_iterator():
         print(cle.array_equal(i,j))
 
         assert cle.array_equal(i,j)
+
+def test_enumerate():
+    import pyclesperanto_prototype as cle
+
+    cle_array = cle.create((2, 10))
+    cle.set_ramp_x(cle_array)
+
+    sum_ = 0
+    for i, y in enumerate(cle_array[0]):
+        print(i, y)
+        sum_ += y
+
+    assert sum_ == 45
+
+def test_zip():
+    import pyclesperanto_prototype as cle
+
+    cle_array = cle.create((2, 10))
+    cle.set_ramp_x(cle_array)
+
+    sum_ = 0
+    for x, y in zip(cle_array[0], cle_array[1]):
+        print(x, y)
+        sum_ += y
+
+    assert sum_ == 45
