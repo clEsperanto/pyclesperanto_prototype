84b17fd9aaffd377994af7737fcbc7ba36cf267e
Robert Haase <haesleinhuepf@users.noreply.github.com>
2023-03-26 14:06:33 +0200

    create result images of type uint8

--- pyclesperanto_prototype/_tier1/_mode_sphere.py
+++ pyclesperanto_prototype/_tier1/_mode_sphere.py
@@ -1,11 +1,11 @@
 import warnings
 
-from .._tier0 import radius_to_kernel_size, execute
+from .._tier0 import radius_to_kernel_size, execute, create_none, create
 from .._tier0 import plugin_function
 from .._tier0 import Image
-import warnings
 
-@plugin_function(categories=['label processing', 'in assistant'])
+
+@plugin_function(categories=['label processing', 'in assistant'], output_creator=create_none)
 def mode_sphere(source : Image, destination : Image = None, radius_x : int = 1, radius_y : int = 1, radius_z : int = 1) -> Image:
     """Computes the local mode of a pixels sphere shaped neighborhood.
     This can be used to post-process and locally correct semantic segmentation results.
@@ -35,6 +35,9 @@ def mode_sphere(source : Image, destination : Image = None, radius_x : int = 1, 
     import numpy as np
     if source.dtype != np.uint8:
         warnings.warn("mode_sphere supports values between 0 and 255 only. Use pixel type uint8 for the source image to mute this warning.")
+
+    if destination is None:
+        destination = create(source, dtype=np.uint8)
 
     kernel_size_x = radius_to_kernel_size(radius_x)
     kernel_size_y = radius_to_kernel_size(radius_y)
