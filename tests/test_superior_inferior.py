26221bba8f2da7428917ec412c9482a481d2c512
Robert Haase <haesleinhuepf@users.noreply.github.com>
2023-04-23 08:25:00 +0200

    more tests comparing with skimage

--- tests/test_superior_inferior.py
+++ tests/test_superior_inferior.py
@@ -1,6 +1,6 @@
 import pyclesperanto_prototype as cle
 import numpy as np
-
+from skimage.segmentation.morphsnakes import sup_inf, inf_sup
 
 def test_superior_inferior_2d():
     test = cle.push(np.asarray([
@@ -38,10 +38,27 @@ def test_superior_inferior_2d():
     b = cle.pull(reference)
     assert (np.array_equal(a, b))
 
-def test_superior_inferior_2d_compare_with_skimage():
-    from skimage.segmentation.morphsnakes import sup_inf, inf_sup
+    reference2 = sup_inf(test)
+    assert cle.array_equal(result, reference2)
+
+def test_superior_inferior_2d_compare_with_skimage_x():
 
     array = np.zeros((100, 85), dtype=np.uint8)
+    array[15:85, 15:85] = 1
+    array[35:65, 35:65] = 0
+
+    result = cle.superior_inferior(cle.inferior_superior(array))
+    reference = sup_inf(inf_sup(array))
+
+    print("result", result)
+    print("reference", reference)
+
+    assert cle.array_equal(result, reference)
+
+
+def test_superior_inferior_2d_compare_with_skimage_y():
+
+    array = np.zeros((85, 100), dtype=np.uint8)
     array[15:85, 15:85] = 1
     array[35:65, 35:65] = 0
 
@@ -87,41 +104,9 @@ def test_superior_inferior_3d():
        [0, 0, 0, 0, 0]],
     ]))
     
-    reference = cle.push(np.asarray([
-        [[0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0]],
+    reference = inf_sup(test)
 
-        [[0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 1, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0]],
-
-       [[0, 0, 0, 0, 0],
-        [0, 0, 1, 0, 0],
-        [0, 1, 0, 1, 0],
-        [0, 0, 1, 0, 0],
-        [0, 0, 0, 0, 0]],
-
-       [[0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 1, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0]],
-
-       [[0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0]],
-
-    ]))
-
-    result = cle.create(test)
-    cle.inferior_superior(test, result)
+    result = cle.inferior_superior(test)
 
     print("result", result)
     print("reference", reference)
@@ -129,3 +114,45 @@ def test_superior_inferior_3d():
     a = cle.pull(result)
     b = cle.pull(reference)
     assert (np.array_equal(a, b))
+
+def test_superior_inferior_3d_compare_with_skimage_x():
+
+    array = np.zeros((5, 5, 4))
+    array[1:4, 1:4, 1:4] = 1
+    array[2, 2, 2] = 0
+
+    result = cle.superior_inferior(cle.inferior_superior(array))
+    reference = sup_inf(inf_sup(array))
+
+    print("result", result)
+    print("reference", reference)
+
+    assert cle.array_equal(result, reference)
+
+def test_superior_inferior_3d_compare_with_skimage_y():
+
+    array = np.zeros((5, 4, 5))
+    array[1:4, 1:4, 1:4] = 1
+    array[2, 2, 2] = 0
+
+    result = cle.superior_inferior(cle.inferior_superior(array))
+    reference = sup_inf(inf_sup(array))
+
+    print("result", result)
+    print("reference", reference)
+
+    assert cle.array_equal(result, reference)
+
+def test_superior_inferior_3d_compare_with_skimage_z():
+
+    array = np.zeros((4, 5, 5))
+    array[1:4, 1:4, 1:4] = 1
+    array[2, 2, 2] = 0
+
+    result = cle.superior_inferior(cle.inferior_superior(array))
+    reference = sup_inf(inf_sup(array))
+
+    print("result", result)
+    print("reference", reference)
+
+    assert cle.array_equal(result, reference)
