c7854c867be109ef75a105d14ebfe5e7f4c0d822
Robert Haase <haesleinhuepf@users.noreply.github.com>
2023-04-23 08:18:33 +0200

    added test directly comparing with skimage

--- tests/test_superior_inferior.py
+++ tests/test_superior_inferior.py
@@ -37,8 +37,23 @@ def test_superior_inferior_2d():
     a = cle.pull(result)
     b = cle.pull(reference)
     assert (np.array_equal(a, b))
-    
-    
+
+def test_superior_inferior_2d_compare_with_skimage():
+    from skimage.segmentation.morphsnakes import sup_inf, inf_sup
+
+    array = np.zeros((100, 85), dtype=np.uint8)
+    array[15:85, 15:85] = 1
+    array[35:65, 35:65] = 0
+
+    result = cle.superior_inferior(cle.inferior_superior(array))
+    reference = sup_inf(inf_sup(array))
+
+    print("result", result)
+    print("reference", reference)
+
+    assert cle.array_equal(result, reference)
+
+
 def test_superior_inferior_3d():
     test = cle.push(np.asarray([
        [[0, 0, 0, 0, 0],
