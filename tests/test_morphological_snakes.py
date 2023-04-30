c3504fd6f1b6cccc2e1fbf6c87ea4e1b67952150
Robert Haase <haesleinhuepf@users.noreply.github.com>
2023-04-30 11:37:29 +0200

    added test

--- tests/test_morphological_snakes.py
+++ tests/test_morphological_snakes.py
@@ -3,13 +3,13 @@ from skimage import morphology
 from skimage import segmentation
 import numpy as np
 
-def test_morphological_snakes():
+def test_morphological_chan_vese():
 
     image = generate_disk((50, 50), 5)
 
     reference = cle.push(segmentation.morphological_chan_vese(image, num_iter=1, smoothing=1, lambda1=1, lambda2=1))
 
-    result = cle.morphological_snakes(image, num_iter=1, smoothing=1, lambda1=1, lambda2=1)
+    result = cle.morphological_chan_vese(image, num_iter=1, smoothing=1, lambda1=1, lambda2=1)
 
     a = cle.pull(result)
     b = cle.pull(reference)
@@ -18,6 +18,25 @@ def test_morphological_snakes():
     print(b)
 
     assert (np.array_equal(a, b))
+
+
+def test_morphological_chan_vese_on_membranes_2d():
+    from skimage.data import cells3d
+    from skimage.segmentation import morphological_chan_vese
+    import pyclesperanto_prototype as cle
+    cle.select_device("TX")
+    print(cle.__version__)
+
+    image2d = cells3d()[30, 0, ...]
+
+    result_gpu = cle.morphological_chan_vese(image2d, num_iter=20, smoothing=10)
+    result_cpu =     morphological_chan_vese(image2d, num_iter=20, smoothing=10)
+
+    from skimage.io import imsave
+    imsave("test1.tif", result_cpu)
+    imsave("test2.tif", result_gpu)
+
+    assert cle.array_equal(result_cpu, result_gpu)
 
 
 def generate_disk(shape, radius):
