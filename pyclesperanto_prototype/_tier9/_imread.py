f2ccf78f6f9f179912f1cefd46604487e2a4da24
Robert Haase <haesleinhuepf@users.noreply.github.com>
2024-03-30 18:14:10 +0100

    deprecated imshow and imread

--- pyclesperanto_prototype/_tier9/_imread.py
+++ pyclesperanto_prototype/_tier9/_imread.py
@@ -1,6 +1,8 @@
 from .._tier0 import Image
 
 def imread(filename : str) -> Image:
+    import warnings
+    warnings.warn("cle.imread is deprecated, use skimage.io.imread instead.")
     from skimage.io import imread as skimread
     image = skimread(filename)
 
