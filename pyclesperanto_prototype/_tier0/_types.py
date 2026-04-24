f79063817d058e9c50ba543f56f4ea226c5d8b23
Robert Haase <haesleinhuepf@users.noreply.github.com>
2026-04-24 13:41:32 +0200

    bugfix: issue with types when pyclesperanto (without prototype) is installed in the same environment

--- pyclesperanto_prototype/_tier0/_types.py
+++ pyclesperanto_prototype/_tier0/_types.py
@@ -4,10 +4,19 @@ import pyopencl as cl
 from typing import Union
 
 try:
-    import pyclesperanto
-    Image = Union[np.ndarray, OCLArray, cl.Image, _OCLImage, pyclesperanto._pyclesperanto._Array]
+    import pyclesperanto_opencl
+    import pyclesperanto_cuda
+    Image = Union[np.ndarray, OCLArray, cl.Image, _OCLImage, pyclesperanto_opencl._pyclesperanto._Array, pyclesperanto_cuda._pyclesperanto._Array]
 except ImportError:
-    Image = Union[np.ndarray, OCLArray, cl.Image, _OCLImage]
+    try:
+        import pyclesperanto_opencl
+        Image = Union[np.ndarray, OCLArray, cl.Image, _OCLImage, pyclesperanto_opencl._pyclesperanto._Array]
+    except ImportError:
+        try:
+            import pyclesperanto_cuda
+            Image = Union[np.ndarray, OCLArray, cl.Image, _OCLImage, pyclesperanto_cuda._pyclesperanto._Array]
+        except ImportError:
+            Image = Union[np.ndarray, OCLArray, cl.Image, _OCLImage]
 
 
 def is_image(object):
@@ -20,4 +29,5 @@ def is_image(object):
                                  "<class 'xarray.core.dataarray.DataArray'>",
                                  "<class 'resource_backed_dask_array.ResourceBackedDaskArray'>",
                                  "<class 'torch.Tensor'>",
-                                 "<class 'pyclesperanto._pyclesperanto._Array'>"]
+                                 "<class 'pyclesperanto_opencl._pyclesperanto._Array'>",
+                                 "<class 'pyclesperanto_cuda._pyclesperanto._Array'>"]
